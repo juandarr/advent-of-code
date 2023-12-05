11c02fa531c8fd0dee938264dd720dc274d5732b
Juan Rios <juanrios@tuta.io>
2023-12-05 00:09:33 -0500

    add: python solution to second problem of day 3 challenges

--- day3/day3-2.py
+++ day3/day3-2.py
@@ -13,13 +13,14 @@ def checkItem(matrix, i,j):
         if (jTmp<0 or jTmp>= len(matrix[iTmp])):
             continue
         if (matrix[iTmp][jTmp] not in digits and matrix[iTmp][jTmp] != '.'):
-            return True
+            if matrix[iTmp][jTmp] =='*':
+                return (iTmp, jTmp)
     return False
 
 
 if __name__=='__main__': 
     lines = read_file("day3-1-input.txt")
-    #linesTest = ['467..114..','...*......','..35..633.','......#...','617*......', '.....+.58.','..592.....','......755.','...$.*....','.664.598..']
+    # linesTest = ['467..114..','...*......','..35..633.','......#...','617*......', '.....+.58.','..592.....','......755.','...$.*....','.664.598..']
     #linesTest2 = ['.......................*......*','...910*...............233..189.','2......391.....789*............','...................983.........','0........106-...............226','.%............................$','...*......$812......812..851...','.99.711.............+.....*....','...........................113.','28*.....411....%...............']
     m = []
     for line in lines:
@@ -27,23 +28,37 @@ if __name__=='__main__':
     i = 0
     j = 0
     net = 0
+    potentialGear = {}
     while (i < len(m)):
         tmp = ''
-        isItem = False
         j = 0
+        gears = set()
         while (j < len(m[i])):
             if m[i][j] in digits:
                 tmp += m[i][j]
-                if not(isItem):
-                    isItem = checkItem(m, i, j)
-                if (j == len(m[i])-1):
-                    if (tmp != '' and isItem):
-                        net += int(tmp)
+                isGear = checkItem(m, i, j)
+                if isGear != False:
+                    gears.add(isGear)
+                if (j == len(m[i]) -1):
+                    if (tmp != '' and len(gears)>0):
+                        for gear in gears:
+                            if gear in potentialGear:
+                                potentialGear[gear].append(int(tmp))
+                            else:
+                                potentialGear[gear] = [int(tmp)]
             else:
-                if (tmp != '' and isItem):
-                    net += int(tmp)
+                if (tmp != '' and len(gears)>0):
+                    for gear in gears:
+                        if gear in potentialGear:
+                            potentialGear[gear].append(int(tmp))
+                        else:
+                            potentialGear[gear] = [int(tmp)]
                 tmp = ''
-                isItem = False
+                gears = set()
             j += 1
         i += 1
+    for gear in potentialGear:
+        if len(potentialGear[gear])==2:
+            net += potentialGear[gear][0]*potentialGear[gear][1]
+            print(potentialGear[gear])
     print(net)
