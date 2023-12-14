372bef1bb61ea72525da3ea917e8da67d4a0f781
Juan Rios <juanrios@tuta.io>
2023-12-13 23:54:50 -0500

    improvement: worked on challenge 1 of day 12, in Python. Need to create recursive program and iterate accordingly

--- day12/day12-1.py
+++ day12/day12-1.py
@@ -9,6 +9,48 @@ def parseInformation(lines):
         tmp = line.strip().split()
         m.append([list(tmp[0]),tmp[1].split(',') ])
     return m
+def findSequence(path, sequence, l):
+    if sequence == []:
+        return 1
+    print('start: ', path, sequence)
+    seqVal = int(sequence[0])
+    c = 0
+    endOfPath = False
+    while c<len(path):
+        if path[c]=='#':
+            while path[c]=='#':
+                l += 1
+                c += 1
+                if c==len(path):
+                    endOfPath = True
+                    break
+            if not(endOfPath):
+                acum += findSequence(path[c:], sequence[1:],  0 )
+        elif path[c]=='.':
+            while (path[c]=='.'):
+                c += 1
+                if c==len(path):
+                    endOfPath = True
+                    break
+        elif path[c]=='?':
+            if l==seqVal:
+                path[c]='.'
+                acum += findSequence(path[c+1:], sequence[1:], 0)
+            elif l==0:
+                for s in ['.','#']:
+                    path[c] = s
+                    if s=='.':
+                        acum += findSequence(path[c+1:], sequence, 0)
+                    elif s=='#':
+                        l += 1
+                        acum += findSequence(path[c:],sequence, l)
+            elif l<seqVal:
+                path[c]='#'
+        c += 1
+    if l==seqVal and endOfPath:
+        return 1 
+    else:
+        return 0
 
 if __name__=='__main__': 
     test =True
@@ -18,4 +60,18 @@ if __name__=='__main__':
         filename = "day12-1-input.txt"
     lines = read_file(filename)
     m= parseInformation(lines)
-    print(m)
+    d =0
+    for row in m:
+        path = row[0]
+        sequence = row[1]
+        print(path, sequence)
+        acum = 0
+        s=findSequence(path, sequence, 0)
+        print(s)
+        d+=1
+        if d>3:
+            break
+    print(ways)
+
+
+
