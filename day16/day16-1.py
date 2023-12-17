7a76a694e03cae6d53506e2b6ae005d9550a4723
Juan Rios <juanrios@tuta.io>
2023-12-17 05:43:35 -0500

    progress: made some progress solving challenge one of day 16, almost there

--- day16/day16-1.py
+++ day16/day16-1.py
@@ -1,3 +1,7 @@
+# import sys
+# sys.setrecursionlimit(10**5)
+from time import sleep
+
 def read_file(filename):
     file = open(filename,'r')
     return file.read()
@@ -18,40 +22,51 @@ def inLimits(i,j):
 def changeDir(d, mirror):
     tmp = []
     if mirror=='\\':
-        tmp = [-1*d[1],-1*d[0]]
+        tmp = (d[1],d[0])
     elif mirror=='/':
-        tmp = [d[1], d[0]]
-    return d
+        tmp = (-1*d[1], -1*d[0])
+    return tuple(tmp)
 
+visited = set()
 
 def runBeam(m, start, dir):
     i = start[0] 
     j = start[1]
     curDir = dir
-    visited = {}
-    net = 0
-    while inLimits(i,j) and (i,j) not in visited:
-        visited[(i,j)] = 1
-        i += curDir[0]
-        j += curDir[1]
+    while inLimits(i,j) and ((i,j),curDir) not in visited:
         if m[i][j]!='.':
             if m[i][j] in ['|','-']:
-                pass
-                # Ramification
+                if m[i][j]=='|' and curDir in [(0,1),(0,-1)]:
+                    visited.union(runBeam(m,[i,j],(-1,0)))
+                    visited.union(runBeam(m,[i,j],(1,0)))
+                    break
+                if m[i][j]=='-' and curDir in [(1,0),(-1,0)]:
+                    visited.union(runBeam(m,[i,j],(0,1)))
+                    visited.union(runBeam(m,[i,j],(0,-1)))
+                    break
             else:
                 curDir = changeDir(curDir, m[i][j])
-        pass
-    return net+len(visited) 
+        visited.add(((i,j),curDir))
+        i += curDir[0]
+        j += curDir[1]
+        print((i,j))
+    return visited 
 
 if __name__=='__main__': 
-    test = True
+    test = False
+    testNumber = 2
     if test:
-        filename = "day16-test-input.txt"
+        filename = "day16-test{0}-input.txt".format(testNumber)
     else:
         filename = "day16-1-input.txt"
     file = read_file(filename)
     m = parseInformation(file)
-    startDir = [1,0]
+    startDir = (0,1)
     start=[0,0]
-    runBeam(m,start,dir)
-    print(m)
+    visited = runBeam(m,start,startDir)
+    final = set()
+    for i in visited:
+        final.add(i[0])
+    print(final,len(final))
+# Too low: 5990
+# Too low: 6005
