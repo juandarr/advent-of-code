c2e616b52076ada3740014354dd2df89cb2e46a0
Juan Rios <juanrios@tuta.io>
2023-12-17 04:39:47 -0500

    add: input and test files, starting script to solve challenge one of day 16, advent of code 2023

--- day16/day16-1.py
+++ day16/day16-1.py
@@ -1,16 +1,47 @@
 def read_file(filename):
     file = open(filename,'r')
-    return file.readlines()
+    return file.read()
 
-def parseInformation(lines):
+def parseInformation(file):
     # Read lines and expand by rows
+    lines = file.split('\n')
     m = []
     for line in lines:
         m.append(list(line.strip()))
     return m
 
-def runBeam(m):
-    return 0
+def inLimits(i,j):
+    if i<0 or i>len(m)-1 or j<0 or j>len(m[0])-1:
+        return False
+    return True
+
+def changeDir(d, mirror):
+    tmp = []
+    if mirror=='\\':
+        tmp = [-1*d[1],-1*d[0]]
+    elif mirror=='/':
+        tmp = [d[1], d[0]]
+    return d
+
+
+def runBeam(m, start, dir):
+    i = start[0] 
+    j = start[1]
+    curDir = dir
+    visited = {}
+    net = 0
+    while inLimits(i,j) and (i,j) not in visited:
+        visited[(i,j)] = 1
+        i += curDir[0]
+        j += curDir[1]
+        if m[i][j]!='.':
+            if m[i][j] in ['|','-']:
+                pass
+                # Ramification
+            else:
+                curDir = changeDir(curDir, m[i][j])
+        pass
+    return net+len(visited) 
 
 if __name__=='__main__': 
     test = True
@@ -18,6 +49,9 @@ if __name__=='__main__':
         filename = "day16-test-input.txt"
     else:
         filename = "day16-1-input.txt"
-    lines = read_file(filename)
-    m = parseInformation(lines)
+    file = read_file(filename)
+    m = parseInformation(file)
+    startDir = [1,0]
+    start=[0,0]
+    runBeam(m,start,dir)
     print(m)
