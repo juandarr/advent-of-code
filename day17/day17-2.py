f8b21ce00509359366e23dfda4c1d11716fda1d7
Juan Rios <juanrios@tuta.io>
2023-12-19 17:52:49 -0500

    add: completed second challenge of day 17, advent of code 2023 in Python

---
+++ day17/day17-2.py
@@ -0,0 +1,72 @@
+from time import sleep, time
+import heapq
+
+def read_file(filename):
+    file = open(filename,'r')
+    return file.read()
+
+def parseInformation(file):
+    # Read lines and expand by rows
+    lines = file.split('\n')
+    m = []
+    for line in lines:
+        m.append(list(line.strip()))
+    return m
+
+def inLimits(i,j,m):
+    if i<0 or i>len(m)-1 or j<0 or j>len(m[0])-1:
+        return False
+    return True
+
+def explore(starts, m, minM):
+    toExpand = []
+    for start in starts:
+        heapq.heappush(toExpand, start)
+    visited = set()
+    t0 = time()
+    while toExpand:
+        curEnergy, curNode, curDir, steps = heapq.heappop(toExpand)
+        if (curNode, curDir, steps) in visited:
+            continue
+        visited.add((curNode, curDir,steps))
+        dir = (curDir, (curDir[1],curDir[0]),(-1*curDir[1], -1*curDir[0]))
+        for d in dir:
+            if steps<4 and curDir!=d:
+                continue
+            nextNode =(curNode[0]+d[0], curNode[1]+d[1])
+            straight = 1
+            if curDir==d:
+                straight +=steps 
+            if not(inLimits(nextNode[0],nextNode[1],m)) or straight>10: 
+                continue    
+            tmp = curEnergy+ int(m[nextNode[0]][nextNode[1]])
+            if tmp<minM[nextNode[0]][nextNode[1]]:
+                minM[nextNode[0]][nextNode[1]] = tmp
+            heapq.heappush(toExpand,(tmp,nextNode,d, straight))
+    return minM[len(m)-1][len(m[0])-1]
+    t = time()-t0
+
+if __name__=='__main__': 
+    test = False
+    testNumber = 1
+    if test:
+        filename = "day17-test{0}-input.txt".format(testNumber)
+    else:
+        filename = "day17-1-input.txt"
+    file = read_file(filename)
+    t0 = time()
+    m = parseInformation(file)
+    minM = []
+    for i in range(len(m)):
+        minM.append([])
+        for j in range(len(m[0])):
+            minM[i].append(float('inf'))
+    minM[0][0] = 0
+    m[0][0]= 0
+    minM[0][1]= int(m[0][1])
+    minM[1][0]= int(m[1][0])
+    s = explore(((int(m[0][1]),(0,1),(0,1),1),(int(m[1][0]),(1,0),(1,0),1)),m, minM)
+    t =time()-t0
+    print(minM)
+    print(s)
+    print('Total duration (secs): {0}'.format(t))
