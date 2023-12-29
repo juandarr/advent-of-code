f03cc7665a85e1787134d9034c01253784aa0a4a
Juan Rios <juanrios@tuta.io>
2023-12-28 20:15:56 -0500

    feat: now the algorithm solves the shortest path problem. The goal is to convert algorithm to solve the longest path. The graph is still acyclic and directed

--- day23/day23-1.py
+++ day23/day23-1.py
@@ -32,7 +32,7 @@ def createGraph(origin,m,graph):
                 if endPoints[m[i][j]]==(-1*d[0],-1*d[1]):
                     continue
             path.append((i,j))
-        cost -=1
+        cost +=1
         if len(path)>1 or m[path[0][0]][path[0][1]] in endPoints:
             break
         cur = path[0]
@@ -42,76 +42,51 @@ def createGraph(origin,m,graph):
     if len(path)==1:
         cur = path[0]
         if cur[0]==len(m)-1:
-            nodes[origin]=[[cur,cost]]
+            graph[origin]=[[cur,cost]]
             return 
         elif m[cur[0]][cur[1]] in endPoints:
             i = cur[0] + endPoints[m[cur[0]][cur[1]]][0]
             j = cur[1] + endPoints[m[cur[0]][cur[1]]][1]
-            cost-=1
-            nodes[origin]=[[(i,j),cost]]
+            cost+=1
+            graph[origin]=[[(i,j),cost]]
             createGraph((i,j),m,graph)
     else:
-        cost -=1
+        cost +=1
         for p in path:
             cur = p
             i = cur[0] + endPoints[m[cur[0]][cur[1]]][0]
             j = cur[1] + endPoints[m[cur[0]][cur[1]]][1]
-            if origin in nodes:
-                nodes[origin].append([(i,j),cost])
+            if origin in graph:
+                graph[origin].append([(i,j),cost])
             else:
-                nodes[origin]=[[(i,j),cost]]
+                graph[origin]=[[(i,j),cost]]
             createGraph((i,j),m,graph)
 
-def shortestPath(m,origin):
+def shortestPath(graph,origin):
     toExplore = []
     heapq.heappush(toExplore, (0,origin))
     visited = {}
-    cost = []
-    dirs = {'n':(-1,0), 's':(1,0), 'w': (0,-1), 'e':(0,1)}
-    pathAllowed = {'.':1, '>':(0,1),'<':(0,-1),'^':(-1,0), 'v':(1,0)}
-    for i in range(len(m)):
-        tmp = []
-        for j in range(len(m[0])):
-            tmp.append(float('inf'))
-        cost.append(tmp)
-    cost[0][1] = 0
+    cost = {}
+    for node in graph:
+        cost[node] =float('inf')
+    cost[origin] = 0
+    cost[(22,21)] = float('inf')
     while toExplore:
         tmp = heapq.heappop(toExplore)
         curCost = tmp[0]
         curNode = tmp[1]
+        if curNode==(22,21):
+            break
         visited[curNode] = 1
-        if m[curNode[0]][curNode[1]] == '.':
-            for dir in dirs:
-                d = dirs[dir]
-                i = curNode[0]+d[0]
-                j = curNode[1]+d[1]
-                if i<0 or i>len(m)-1:
-                    continue
-                if j<0 or j>len(m[0])-1:
-                    continue
-                if m[i][j] in pathAllowed:
-                    c = curCost-1
-                    if cost[i][j]>c:
-                        cost[i][j] = c
-                    if (i,j) not in visited:
-                        heapq.heappush(toExplore,(cost[i][j], (i,j)))
-        else:
-            d = pathAllowed[m[curNode[0]][curNode[1]]]
-            i = curNode[0]+d[0]
-            j = curNode[1]+d[1]
-            if i<0 or i>len(m)-1:
-                continue
-            if j<0 or j>len(m[0])-1:
-                continue
-
-            if m[i][j] in pathAllowed:
-                c = curCost-1
-                if cost[i][j]>c:
-                    cost[i][j] = c
-                if (i,j) not in visited:
-                    heapq.heappush(toExplore,(cost[i][j], (i,j)))
+        for pair in graph[curNode]: 
+            node = pair[0]
+            c = pair[1]
+            newCost = curCost+c
+            if cost[node]>newCost:
+                cost[node] = newCost
+            if node not in visited:
+                heapq.heappush(toExplore,(newCost, node))
     return cost
-
 
 if __name__=='__main__': 
     test =True
@@ -128,15 +103,15 @@ if __name__=='__main__':
     m = parseInformation(lines)
     print(m)
     origin = (0,1)
-    # p = shortestPath(m,(0,1))
     graph= {}
-    createGraph((0,1),m,graph)
+    createGraph(origin,m,graph)
     # Graph is in format: source:[[dest1, cost1], [dest2, cost2],...]
-    print(len(graph))
-    endPoints = {'>':(0,1),'<':(0,-1),'^':(-1,0), 'v':(1,0)}
-    ed =set()
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            if m[i][j] in endPoints:
-                ed.add((i+endPoints[m[i][j]][0], j+endPoints[m[i][j]][1]))
-    print(len(ed)+1)
+    # print(len(graph))
+    # endPoints = {'>':(0,1),'<':(0,-1),'^':(-1,0), 'v':(1,0)}
+    # ed =set()
+    # for i in range(len(m)):
+    #     for j in range(len(m[0])):
+    #         if m[i][j] in endPoints:
+    #             ed.add((i+endPoints[m[i][j]][0], j+endPoints[m[i][j]][1]))
+    p = shortestPath(graph,origin)
+    print(p)
