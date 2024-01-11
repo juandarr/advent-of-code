67cd2c40de3fc0f4f1b1674fd462a849f81d5b54
Juan Ríos <juanrios@tuta.io>
2024-01-10 21:39:48 -0500

    add: solution to challenge 2, day 3 of advent of code 2022

--- 2022/day3/day3-2.py
+++ 2022/day3/day3-2.py
@@ -1,25 +1,40 @@
-fileSacks = open('day3.txt', 'r')
-ruckSacks = fileSacks.readlines()
+def parseInformation(filename):
+    file = open(filename, "r")
+    tmp= file.read()
+    tmp= tmp.rstrip().split('\n')
+    return tmp
 
-priorities = {}
-p = 1
-for i in range(97, 97+26):
-    priorities[chr(i)] = p
-    p += 1
-for i in range(65, 65+26):
-    priorities[chr(i)] = p
-    p += 1
+def checkGroups(rucksacks):
+    netPriorities = 0
+    items = []
+    for rucksack in rucksacks:
+        items.append(set(rucksack))
+        if len(items) == 3:
+            for a in items[0]:
+                if a in items[1] and a in items[2]:
+                    netPriorities += priorities[a]
+                    break
+            items = []
+    return netPriorities
 
-netPriorities = 0
-items = []
-for ruckSack in ruckSacks:
-    ruckSack = ruckSack.strip('\n')
-    items.append(set(ruckSack))
-    if len(items) == 3:
-        for a in items[0]:
-            if a in items[1] and a in items[2]:
-                netPriorities += priorities[a]
-                break
-        items = []
+if __name__=='__main__': 
+    test = False
+    if test:
+        filename = "day3-test-input.txt"
+    else:
+        filename = "day3-input.txt"
+    # Create the priorities hash map: first a-z, then A-Z
+    priorities = {}
+    p = 1
+    for i in range(97, 97+26):
+        priorities[chr(i)] = p
+        p += 1
+    for i in range(65, 65+26):
+        priorities[chr(i)] = p
+        p += 1
 
-print("Addition of priorities is {0}".format(netPriorities))
+    rucksacks = parseInformation(filename)
+    priorities = checkGroups(rucksacks)
+    print("Addition of priorities is {0}".format(priorities))
+
+
