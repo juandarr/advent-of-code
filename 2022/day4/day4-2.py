5e462216318f01feecff2365cbb005657fdbdc79
Juan Ríos <juanrios@tuta.io>
2024-01-11 18:18:23 -0500

    add: completed challenge second, day 4 of advent of code 2022

--- 2022/day4/day4-2.py
+++ 2022/day4/day4-2.py
@@ -1,15 +1,28 @@
-filePairs = open('day4.txt','r')
-pairs= filePairs.readlines()
+def parseInformation(filename):
+    file = open(filename, "r")
+    tmp= file.read()
+    tmp= tmp.rstrip().split('\n')
+    return tmp
 
-overlap = 0 
+def checkPartialOverlap(ranges):
+    overlaps = 0
+    for pair in ranges:
+        # Format data for adequate comparison
+        elfs = pair.split(',')
+        for idx, elf in enumerate(elfs):
+            elf = elf.split('-')
+            elfs[idx] = [int(elf[0]), int(elf[1])]
+        # If one range is inside the other range, increase variable overlaps
+        if not((elfs[0][0]<elfs[1][0] and elfs[0][1]<elfs[1][0]) or (elfs[0][0]>elfs[1][1] and elfs[0][1]>elfs[1][1])):
+            overlaps += 1
+    return overlaps
 
-for pair in pairs:
-    pair = pair.strip('\n')
-    elfs = pair.split(',')
-    for idx,elf in enumerate(elfs):
-        elf=elf.split('-')
-        elfs[idx]=[int(elf[0]), int(elf[1])]
-    if not((elfs[0][0]<elfs[1][0] and elfs[0][1]<elfs[1][0]) or (elfs[0][0]>elfs[1][1] and elfs[0][1]>elfs[1][1])):
-        overlap += 1
-        print(elfs)
-print("The number of overlaps in work assignments is {0}".format(overlap))
+if __name__=='__main__': 
+    test = False
+    if test:
+        filename = "day4-test-input.txt"
+    else:
+        filename = "day4-input.txt"
+    ranges = parseInformation(filename)
+    overlaps = checkPartialOverlap(ranges)
+    print("The number of partial overlaps in work assignments is {0}".format(overlaps))
