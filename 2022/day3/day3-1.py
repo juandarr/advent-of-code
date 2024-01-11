57a1c0d43fdf2bba2ad2da54491ce43c6773d3d9
Juan Ríos <juanrios@tuta.io>
2024-01-10 21:42:09 -0500

    refactor: added priorities function to encapsule hash map storing priority values

--- 2022/day3/day3-1.py
+++ 2022/day3/day3-1.py
@@ -4,6 +4,17 @@ def parseInformation(filename):
     tmp= tmp.rstrip().split('\n')
     return tmp
 
+def getPriorities():
+    priorities = {}
+    p = 1
+    for i in range(97, 97+26):
+        priorities[chr(i)] = p
+        p += 1
+    for i in range(65, 65+26):
+        priorities[chr(i)] = p
+        p += 1
+    return priorities
+
 def checkRucksacks(rucksacks):
     netPriorities = 0
     for rucksack in rucksacks:
@@ -24,15 +35,7 @@ if __name__=='__main__':
     else:
         filename = "day3-input.txt"
     # Create the priorities hash map: first a-z, then A-Z
-    priorities = {}
-    p = 1
-    for i in range(97, 97+26):
-        priorities[chr(i)] = p
-        p += 1
-    for i in range(65, 65+26):
-        priorities[chr(i)] = p
-        p += 1
-
+    priorities = getPriorities()
     rucksacks = parseInformation(filename)
     priorities = checkRucksacks(rucksacks)
     print("Addition of priorities is {0}".format(priorities))
