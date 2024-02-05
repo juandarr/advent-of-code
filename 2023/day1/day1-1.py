603c893d6a68a72bc68341279976bac23495ca77
Juan Ríos <juanrios@tuta.io>
2024-02-04 19:03:46 -0500

    refactor: solutions to problem one and two of day 1 redefined to comply with aoc2023 requirements;add: relocated .gitignore file to root folder

--- 2023/day1/day1-1.py
+++ 2023/day1/day1-1.py
@@ -1,21 +1,47 @@
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
     return open(filename, "r")
 
-if __name__=='__main__': 
-    lines = read_file("day1-1-input.txt")
-    digits = set([0,1,2,3,4,5,6,7,8,9])
+
+def addValues(lines):
+    digits = set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     net = 0
     for line in lines:
         values = []
         for s in line:
             try:
-                if int(s) in digits: 
+                if int(s) in digits:
                     values.append(int(s))
-            except: 
+            except:
                 continue
-        print(values)
-        val = int('{0}{1}'.format(values[0], values[-1]))
-        net  += val 
-        print(val)
-    print(net)
+        if len(values) > 0:
+            val = int("{0}{1}".format(values[0], values[-1]))
+            net += val
+    return net
+
+
+def main(filename):
+    lines = parseInformation(filename)
+    net = addValues(lines)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(1, [142, 209], main)
+    else:
+        biggest = getAnswer(1, main)
+        print("The sum of the calibration values is: {0}".format(biggest))
--- 2023/day1/day1-2.py
+++ 2023/day1/day1-2.py
@@ -1,36 +1,70 @@
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
     return open(filename, "r")
 
-if __name__=='__main__': 
-    lines = read_file("day1-1-input.txt")
-    #linesTest = ['two1nine','eightwothree','abcone2threexyz', 'xtwone3four','4nineeightseven2','zoneight234','7pqrstsixteen']
-    digits = '123456789'
-    digitsWords = {'one':'1','two':'2', 'three':'3','four':'4','five':'5', 'six':'6','seven':'7','eight':'8', 'nine':'9'}
-    for dw in digitsWords:
-        print(dw[::-1])
+
+def addValues(lines):
+    digits = "123456789"
+    digitsWords = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
     net = 0
     c = 0
     for line in lines:
         start = [float("inf"), None]
         for d in digits:
-            tmp = line.find(d) 
-            if tmp>=0 and tmp < start[0]:
+            tmp = line.find(d)
+            if tmp >= 0 and tmp < start[0]:
                 start = [tmp, d]
         for dw in digitsWords:
-            tmp = line.find(dw) 
-            if tmp>=0 and tmp < start[0]:
+            tmp = line.find(dw)
+            if tmp >= 0 and tmp < start[0]:
                 start = [tmp, digitsWords[dw]]
         end = [float("inf"), None]
         lineReversed = line[::-1]
         for d in digits:
-            tmp = lineReversed.find(d) 
-            if tmp>=0 and tmp < end[0]:
+            tmp = lineReversed.find(d)
+            if tmp >= 0 and tmp < end[0]:
                 end = [tmp, d]
         for dw in digitsWords:
-            tmp = lineReversed.find(dw[::-1]) 
-            if tmp>=0 and tmp+len(dw)-1 < end[0]:
-                end = [tmp+len(dw)-1, digitsWords[dw]]
-        val = int('{0}{1}'.format(start[1], end[1]))
-        net  += val 
-    print(net)
+            tmp = lineReversed.find(dw[::-1])
+            if tmp >= 0 and tmp + len(dw) - 1 < end[0]:
+                end = [tmp + len(dw) - 1, digitsWords[dw]]
+        val = int("{0}{1}".format(start[1], end[1]))
+        net += val
+    return net
+
+
+def main(filename):
+    lines = parseInformation(filename)
+    net = addValues(lines)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(1, [142, 281], main)
+    else:
+        biggest = getAnswer(1, main)
+        print("The sum of the calibration values is: {0}".format(biggest))
