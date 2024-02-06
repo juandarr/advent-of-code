586894a50bc36359f33acfa1b2137e8fb5886281
Juan Ríos <juanrios@tuta.io>
2024-02-06 16:16:02 -0500

    refactor: adapted solutions of day 3 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day3/day3-1.py
+++ 2023/day3/day3-1.py
@@ -1,49 +1,86 @@
-digits = {'0': True,'1': True,'2':True,'3':True,'4':True,'5': True,'6': True,'7': True,'8': True,'9':True}
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+digits = {
+    "0": True,
+    "1": True,
+    "2": True,
+    "3": True,
+    "4": True,
+    "5": True,
+    "6": True,
+    "7": True,
+    "8": True,
+    "9": True,
+}
+
+
+def parseInformation(filename):
     return open(filename, "r")
 
-def checkItem(matrix, i,j):
-    dirs = [(-1,-1),(-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+
+def checkItem(matrix, i, j):
+    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for dir in dirs:
         iTmp = i + dir[1]
-        if (iTmp<0 or iTmp>=len(matrix)):
+        if iTmp < 0 or iTmp >= len(matrix):
             continue
         jTmp = j + dir[0]
-        if (jTmp<0 or jTmp>= len(matrix[iTmp])):
+        if jTmp < 0 or jTmp >= len(matrix[iTmp]):
             continue
-        if (matrix[iTmp][jTmp] not in digits and matrix[iTmp][jTmp] != '.'):
+        if matrix[iTmp][jTmp] not in digits and matrix[iTmp][jTmp] != ".":
             return True
     return False
 
 
-if __name__=='__main__': 
-    lines = read_file("day3-1-input.txt")
-    #linesTest = ['467..114..','...*......','..35..633.','......#...','617*......', '.....+.58.','..592.....','......755.','...$.*....','.664.598..']
-    #linesTest2 = ['.......................*......*','...910*...............233..189.','2......391.....789*............','...................983.........','0........106-...............226','.%............................$','...*......$812......812..851...','.99.711.............+.....*....','...........................113.','28*.....411....%...............']
+def sumPartNumbers(lines):
     m = []
     for line in lines:
         m.append(list(line.strip()))
     i = 0
     j = 0
     net = 0
-    while (i < len(m)):
-        tmp = ''
+    while i < len(m):
+        tmp = ""
         isItem = False
         j = 0
-        while (j < len(m[i])):
+        while j < len(m[i]):
             if m[i][j] in digits:
                 tmp += m[i][j]
-                if not(isItem):
+                if not (isItem):
                     isItem = checkItem(m, i, j)
-                if (j == len(m[i])-1):
-                    if (tmp != '' and isItem):
+                if j == len(m[i]) - 1:
+                    if tmp != "" and isItem:
                         net += int(tmp)
             else:
-                if (tmp != '' and isItem):
+                if tmp != "" and isItem:
                     net += int(tmp)
-                tmp = ''
+                tmp = ""
                 isItem = False
             j += 1
         i += 1
-    print(net)
+    return net
+
+
+def main(filename):
+    lines = parseInformation(filename)
+    net = sumPartNumbers(lines)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 3, [4361], main)
+    else:
+        ans = getAnswer(2023, 3, main)
+        print("The sum of all part numbers is: {0}".format(ans))
--- 2023/day3/day3-2.py
+++ 2023/day3/day3-2.py
@@ -1,27 +1,43 @@
-digits = {'0': True,'1': True,'2':True,'3':True,'4':True,'5': True,'6': True,'7': True,'8': True,'9':True}
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+digits = {
+    "0": True,
+    "1": True,
+    "2": True,
+    "3": True,
+    "4": True,
+    "5": True,
+    "6": True,
+    "7": True,
+    "8": True,
+    "9": True,
+}
+
+
+def parseInformation(filename):
     return open(filename, "r")
 
-def checkItem(matrix, i,j):
-    dirs = [(-1,-1),(-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+
+def checkItem(matrix, i, j):
+    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for dir in dirs:
         iTmp = i + dir[1]
-        if (iTmp<0 or iTmp>=len(matrix)):
+        if iTmp < 0 or iTmp >= len(matrix):
             continue
         jTmp = j + dir[0]
-        if (jTmp<0 or jTmp>= len(matrix[iTmp])):
+        if jTmp < 0 or jTmp >= len(matrix[iTmp]):
             continue
-        if (matrix[iTmp][jTmp] not in digits and matrix[iTmp][jTmp] != '.'):
-            if matrix[iTmp][jTmp] =='*':
+        if matrix[iTmp][jTmp] not in digits and matrix[iTmp][jTmp] != ".":
+            if matrix[iTmp][jTmp] == "*":
                 return (iTmp, jTmp)
     return False
 
 
-if __name__=='__main__': 
-    lines = read_file("day3-1-input.txt")
-    # linesTest = ['467..114..','...*......','..35..633.','......#...','617*......', '.....+.58.','..592.....','......755.','...$.*....','.664.598..']
-    #linesTest2 = ['.......................*......*','...910*...............233..189.','2......391.....789*............','...................983.........','0........106-...............226','.%............................$','...*......$812......812..851...','.99.711.............+.....*....','...........................113.','28*.....411....%...............']
+def sumGearRatios(lines):
     m = []
     for line in lines:
         m.append(list(line.strip()))
@@ -29,36 +45,56 @@ if __name__=='__main__':
     j = 0
     net = 0
     potentialGear = {}
-    while (i < len(m)):
-        tmp = ''
+    while i < len(m):
+        tmp = ""
         j = 0
         gears = set()
-        while (j < len(m[i])):
+        while j < len(m[i]):
             if m[i][j] in digits:
                 tmp += m[i][j]
                 isGear = checkItem(m, i, j)
-                if isGear != False:
+                if isGear is not False:
                     gears.add(isGear)
-                if (j == len(m[i]) -1):
-                    if (tmp != '' and len(gears)>0):
+                if j == len(m[i]) - 1:
+                    if tmp != "" and len(gears) > 0:
                         for gear in gears:
                             if gear in potentialGear:
                                 potentialGear[gear].append(int(tmp))
                             else:
                                 potentialGear[gear] = [int(tmp)]
             else:
-                if (tmp != '' and len(gears)>0):
+                if tmp != "" and len(gears) > 0:
                     for gear in gears:
                         if gear in potentialGear:
                             potentialGear[gear].append(int(tmp))
                         else:
                             potentialGear[gear] = [int(tmp)]
-                tmp = ''
+                tmp = ""
                 gears = set()
             j += 1
         i += 1
     for gear in potentialGear:
-        if len(potentialGear[gear])==2:
-            net += potentialGear[gear][0]*potentialGear[gear][1]
-            print(potentialGear[gear])
-    print(net)
+        if len(potentialGear[gear]) == 2:
+            net += potentialGear[gear][0] * potentialGear[gear][1]
+    return net
+
+
+def main(filename):
+    lines = parseInformation(filename)
+    net = sumGearRatios(lines)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 3, [467835], main)
+    else:
+        ans = getAnswer(2023, 3, main)
+        print("The sum of gear ratios is: {0}".format(ans))
