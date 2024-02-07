a224eafb868e2afe98f5b9fad0523247304f9d55
Juan Ríos <juanrios@tuta.io>
2024-02-07 12:10:28 -0500

    refactor: adapted solutions of day 4 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day4/day4-1.py
+++ 2023/day4/day4-1.py
@@ -1,25 +1,52 @@
-def read_file(filename):
-    return open(filename, "r")
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(line):
-    tmp = line.strip().split(':')
-    numberSets = tmp[1].strip().split('|')
-    winnerSet = set(numberSets[0].strip().split())
-    candidates = numberSets[1].strip().split()
-    print(winnerSet, candidates)
-    matches = 0
-    for candidate in candidates:
-        if candidate in winnerSet:
-            matches += 1
-    if matches>0:
-        return 2**(matches-1)
-    else: 
-        return 0
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-if __name__=='__main__': 
-    lines = read_file("day4-1-input.txt")
-    #linesTest= ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53','Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19','Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1','Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83','Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36','Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
-    net = 0
+
+def parseInformation(filename):
+    lines = open(filename, "r")
+    candidateList = []
+    winnerList = []
     for line in lines:
-        net += parseInformation(line)
-    print(net)
+        tmp = line.strip().split(":")
+        numberSets = tmp[1].strip().split("|")
+        winnerSet = set(numberSets[0].strip().split())
+        candidates = numberSets[1].strip().split()
+        candidateList.append(candidates)
+        winnerList.append(winnerSet)
+    return candidateList, winnerList
+
+
+def score(candidateList, winnerList):
+    score = 0
+    for idx, candidates in enumerate(candidateList):
+        matches = 0
+        for candidate in candidates:
+            if candidate in winnerList[idx]:
+                matches += 1
+        if matches > 0:
+            score += 2 ** (matches - 1)
+    return score
+
+
+def main(filename):
+    candidateList, winnerList = parseInformation(filename)
+    net = score(candidateList, winnerList)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 4, [13], main)
+    else:
+        ans = getAnswer(2023, 4, main)
+        print("The score is: {0}".format(ans))
--- 2023/day4/day4-2.py
+++ 2023/day4/day4-2.py
@@ -1,9 +1,18 @@
-def read_file(filename):
-    return open(filename, "r")
+from os.path import dirname, abspath
+import sys
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    lines = open(filename, "r")
+    return lines
+
 
 def getScore(line):
-    tmp = line.strip().split(':')
-    numberSets = tmp[1].strip().split('|')
+    tmp = line.strip().split(":")
+    numberSets = tmp[1].strip().split("|")
     winnerSet = set(numberSets[0].strip().split())
     candidates = numberSets[1].strip().split()
     matches = 0
@@ -12,31 +21,52 @@ def getScore(line):
             matches += 1
     return matches
 
-def replicate(copies, cards):
+
+def replicate(scores, copies, cards):
     for card in cards:
         base = card
         for _ in range(scores[card]):
             base += 1
             if base > len(scores):
                 break
-            cards[base] += copies[card] 
-            copies[base] += copies[card] 
-        copies[card] = 0 
-    
-if __name__=='__main__': 
-    lines = read_file("day4-1-input.txt")
-    #linesTest= ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53','Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19','Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1','Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83','Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36','Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
+            cards[base] += copies[card]
+            copies[base] += copies[card]
+        copies[card] = 0
+
+
+def copyAndScore(lines):
     c = 1
     scores = {}
     cards = {}
     copies = {}
-    for line in lines: 
+    for line in lines:
         scores[c] = getScore(line)
-        cards[c] = 1 
+        cards[c] = 1
         copies[c] = 1
         c += 1
-    replicate(copies, cards)
+    replicate(scores, copies, cards)
     net = 0
     for card in cards:
         net += cards[card]
-    print(net)
+    return net
+
+
+def main(filename):
+    lines = parseInformation(filename)
+    net = copyAndScore(lines)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 4, [30], main)
+    else:
+        ans = getAnswer(2023, 4, main)
+        print("The total scratchcards is: {0}".format(ans))
