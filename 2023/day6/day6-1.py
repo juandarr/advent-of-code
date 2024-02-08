2748274ac86c8418160b1e3e39859fdf77cd572d
Juan Ríos <juanrios@tuta.io>
2024-02-08 12:31:33 -0500

    refactor: adapted solutions of day 6 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day6/day6-1.py
+++ 2023/day6/day6-1.py
@@ -1,33 +1,55 @@
-def read_file(filename):
-    return open(filename, "r")
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    lines = open(filename, "r")
     c = 0
+    times = []
+    distances = []
     for line in lines:
-        if c==0:
-            time = line.strip().split(':')[1].strip().split()
+        if c == 0:
+            times = line.strip().split(":")[1].strip().split()
             c += 1
         else:
-            distance = line.strip().split(':')[1].strip().split()
-    return time,distance
+            distances = line.strip().split(":")[1].strip().split()
+    return times, distances
 
-if __name__=='__main__': 
-    test =False 
-    if test:
-        filename = "day6-test-input.txt"
-    else:
-        filename = "day6-1-input.txt"
-    lines = read_file(filename)
-    times,distances = parseInformation(lines)
+
+def waysToBeatRecord(times, distances):
     c = 0
     net = 1
-    while (c<len(times)):
+    while c < len(times):
         time = int(times[c])
-        distance =int(distances[c])
+        distance = int(distances[c])
         ways = 0
-        for t in range(1,time):
-            if (t*(time-t)>distance):
+        for t in range(1, time):
+            if t * (time - t) > distance:
                 ways += 1
         net *= ways
         c += 1
-    print(net)
+    return net
+
+
+def main(filename):
+    times, distances = parseInformation(filename)
+    mul = waysToBeatRecord(times, distances)
+    return mul
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 6, [288], main)
+    else:
+        ans = getAnswer(2023, 6, main)
+        print("The product of ways to beat record is: {0}".format(ans))
--- 2023/day6/day6-2.py
+++ 2023/day6/day6-2.py
@@ -1,48 +1,70 @@
-from math import sqrt,ceil,floor
-def read_file(filename):
-    return open(filename, "r")
+from math import sqrt, ceil, floor
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    lines = open(filename, "r")
     c = 0
+    time = []
+    distance = []
     for line in lines:
-        if c==0:
-            time = line.strip().split(':')[1].strip().split()
+        if c == 0:
+            time = line.strip().split(":")[1].strip().split()
             c += 1
         else:
-            distance = line.strip().split(':')[1].strip().split()
-    return time,distance
+            distance = line.strip().split(":")[1].strip().split()
+    return time, distance
 
-if __name__=='__main__': 
-    test =False 
-    if test:
-        filename = "day6-test-input.txt"
-    else:
-        filename = "day6-1-input.txt"
-    lines = read_file(filename)
-    times,distances = parseInformation(lines)
-    times = [''.join(times)]
-    distances = [''.join(distances)]
-    print(times,distances)
+
+def waysToBeatRecord(times, distances):
+    times = ["".join(times)]
+    distances = ["".join(distances)]
+    print(times, distances)
     c = 0
     net = 1
-    while (c<len(times)):
+    while c < len(times):
         time = int(times[c])
-        distance =int(distances[c])
+        distance = int(distances[c])
         ways = 0
-        x2 = (time + sqrt(time**2 - 4*distance))/2.0
-        x1= (time - sqrt(time**2 - 4*distance))/2.0
+        x2 = (time + sqrt(time**2 - 4 * distance)) / 2.0
+        x1 = (time - sqrt(time**2 - 4 * distance)) / 2.0
         # If solution is integer increase the value
-        if x1==int(x1):
-            x1 = int(x1)+1
+        if x1 == int(x1):
+            x1 = int(x1) + 1
         # Else take the ceil of the float
         else:
             x1 = ceil(x1)
         # If solution is integer increase the value
-        if x2==int(x2):
-            x2 = int(x2)-1
+        if x2 == int(x2):
+            x2 = int(x2) - 1
         # Else take the ceil of the float
         else:
             x2 = floor(x2)
-        net *= x2-x1+1
+        net *= x2 - x1 + 1
         c += 1
-    print(net)
+    return net
+
+
+def main(filename):
+    times, distances = parseInformation(filename)
+    ways = waysToBeatRecord(times, distances)
+    return ways
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 6, [71503], main)
+    else:
+        ans = getAnswer(2023, 6, main)
+        print("The number of ways to beat record is: {0}".format(ans))
