ccfc0ef64465bb9d4ac4a077978514d66819083a
Juan Ríos <juanrios@tuta.io>
2024-02-10 10:27:56 -0500

    refactor: adapted solutions of day 11 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day11/day11-1.py
+++ 2023/day11/day11-1.py
@@ -1,14 +1,19 @@
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
-    m =[]
-    expansion = 2 
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    m = []
+    expansion = 2
     # Read lines and expand by rows
     for line in lines:
         tmp = line.strip()
-        if tmp == '.'*len(tmp):
+        if tmp == "." * len(tmp):
             for _ in range(expansion):
                 m.append(list(tmp))
         else:
@@ -19,38 +24,53 @@ def parseInformation(lines):
     j = 0
     galaxies = []
     # Expand by columns and store location of galaxies
-    while (j<len(m[0])):
+    while j < len(m[0]):
         allDots = True
         for i in range(len(m)):
-            if m[i][j]!='.':
+            if m[i][j] != ".":
                 allDots = False
                 break
         if allDots:
             for i in range(len(expanded)):
                 for _ in range(expansion):
-                    expanded[i].append('.')
+                    expanded[i].append(".")
         else:
             for i in range(len(expanded)):
                 expanded[i].append(m[i][j])
-                if expanded[i][-1]=='#':
-                    galaxies.append((i,len(expanded[i])-1))
+                if expanded[i][-1] == "#":
+                    galaxies.append((i, len(expanded[i]) - 1))
         j += 1
-    return m,galaxies
+    return m, galaxies
 
-if __name__=='__main__': 
-    test = False
-    if test:
-        filename = "day11-test-input.txt"
-    else:
-        filename = "day11-1-input.txt"
-    lines = read_file(filename)
-    m,galaxies= parseInformation(lines)
+
+def sumShortestPath(m, galaxies):
     net = 0
-    for idx,galaxy in enumerate(galaxies):
+    for idx, galaxy in enumerate(galaxies):
         i = idx + 1
-        while (i<len(galaxies)):
+        while i < len(galaxies):
             galaxy2 = galaxies[i]
-            d = abs(galaxy[0]-galaxy2[0]) + abs(galaxy[1] - galaxy2[1]) 
+            d = abs(galaxy[0] - galaxy2[0]) + abs(galaxy[1] - galaxy2[1])
             net += d
             i += 1
-    print(net)
+    return net
+
+
+def main(filename):
+    m, galaxies = parseInformation(filename)
+    net = sumShortestPath(m, galaxies)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 11, [374], main)
+    else:
+        ans = getAnswer(2023, 11, main)
+        print("The sum of shortest paths is: {0}".format(ans))
--- 2023/day11/day11-2.py
+++ 2023/day11/day11-2.py
@@ -1,18 +1,23 @@
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
-    m =[]
-    r= []
-    # Read lines 
-    row= 0
-    expansionFactor = 10**6 
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename, rows):
+    file = open(filename, "r")
+    lines = file.readlines()
+    m = []
+    r = []
+    # Read lines
+    row = 0
+    expansionFactor = rows
     # The effective expansion, since the original row and column are preserved
-    expansion = expansionFactor-1
+    expansion = expansionFactor - 1
     for line in lines:
         tmp = line.strip()
-        if tmp == '.'*len(tmp):
+        if tmp == "." * len(tmp):
             r.append(row)
         m.append(list(tmp))
         row += 1
@@ -20,35 +25,55 @@ def parseInformation(lines):
     c = []
     j = 0
     # Expand by columns and store location of galaxies
-    while (j<len(m[0])):
+    while j < len(m[0]):
         allDots = True
         for i in range(len(m)):
-            if m[i][j]!='.':
+            if m[i][j] != ".":
                 allDots = False
                 break
         if allDots:
             c.append(j)
         else:
             for i in range(len(m)):
-                if m[i][j]=='#':
-                    galaxies.append((i+expansion*len([val for val in r if val<i]),j+expansion*len([val for val in c if val<j])))
+                if m[i][j] == "#":
+                    galaxies.append(
+                        (
+                            i + expansion * len([val for val in r if val < i]),
+                            j + expansion * len([val for val in c if val < j]),
+                        )
+                    )
         j += 1
-    return m,galaxies
+    return m, galaxies
 
-if __name__=='__main__': 
-    test =False
-    if test:
-        filename = "day11-test-input.txt"
-    else:
-        filename = "day11-1-input.txt"
-    lines = read_file(filename)
-    m,galaxies= parseInformation(lines)
+
+def sumShortestPath(m, galaxies):
     net = 0
-    for idx,galaxy in enumerate(galaxies):
+    for idx, galaxy in enumerate(galaxies):
         i = idx + 1
-        while (i<len(galaxies)):
+        while i < len(galaxies):
             galaxy2 = galaxies[i]
-            d = abs(galaxy[0]-galaxy2[0]) + abs(galaxy[1] - galaxy2[1]) 
+            d = abs(galaxy[0] - galaxy2[0]) + abs(galaxy[1] - galaxy2[1])
             net += d
             i += 1
-    print(net)
+    return net
+
+
+def main(filename, rows):
+    m, galaxies = parseInformation(filename, rows)
+    net = sumShortestPath(m, galaxies)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 11, [8410], main, 100)
+    else:
+        ans = getAnswer(2023, 11, main)
+        print("The sum of shortest paths is: {0}".format(ans))
