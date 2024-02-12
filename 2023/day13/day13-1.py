d4211c111c1f514352cc877862a13472b8609d62
Juan Ríos <juanrios@tuta.io>
2024-02-12 08:22:18 -0500

    refactor: adapted solutions of day 13 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day13/day13-1.py
+++ 2023/day13/day13-1.py
@@ -1,13 +1,18 @@
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
-    patterns =[]
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    patterns = []
     tmp = []
     # Read lines and expand by rows
     for line in lines:
-        if line.strip()=='':
+        if line.strip() == "":
             patterns.append(tmp)
             tmp = []
         else:
@@ -15,21 +20,22 @@ def parseInformation(lines):
     patterns.append(tmp)
     return patterns
 
+
 def findMirror(pattern):
     i = 0
     mirrorFound = False
     mirrors = {}
-    while i<len(pattern)-1:
-        e = len(pattern)-1
-        while (e>i):
-            if pattern[i]==pattern[e] and (i==0 or e==len(pattern)-1):
+    lines = []
+    while i < len(pattern) - 1:
+        e = len(pattern) - 1
+        while e > i:
+            if pattern[i] == pattern[e] and (i == 0 or e == len(pattern) - 1):
                 iTmp = i
                 eTmp = e
-                lines = []
-                while (pattern[iTmp]==pattern[eTmp]):
-                    if (eTmp-iTmp)==1:
+                while pattern[iTmp] == pattern[eTmp]:
+                    if (eTmp - iTmp) == 1:
                         lines.append(iTmp)
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         lines.append(iTmp)
@@ -37,64 +43,78 @@ def findMirror(pattern):
                     eTmp -= 1
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         i += 1
     if mirrorFound:
-        mirrors['h'] = lines[-1]+1
+        mirrors["h"] = lines[-1] + 1
     j = 0
     mirrorFound = False
-    while j<len(pattern[0])-1:
-        e = len(pattern[0])-1
-        while (e>j):
-            colJ = ''.join([pattern[i][j] for i in range(len(pattern))])
-            colE = ''.join([pattern[i][e] for i in range(len(pattern))])
-            if colJ==colE and (j==0 or e==len(pattern[0])-1):
+    cols = []
+    while j < len(pattern[0]) - 1:
+        e = len(pattern[0]) - 1
+        while e > j:
+            colJ = "".join([pattern[i][j] for i in range(len(pattern))])
+            colE = "".join([pattern[i][e] for i in range(len(pattern))])
+            if colJ == colE and (j == 0 or e == len(pattern[0]) - 1):
                 jTmp = j
                 eTmp = e
-                cols = []
-                while (colJ==colE):
-                    if (eTmp-jTmp)==1 :
+                while colJ == colE:
+                    if (eTmp - jTmp) == 1:
                         cols.append(jTmp)
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         cols.append(jTmp)
                     jTmp += 1
                     eTmp -= 1
-                    if (jTmp-eTmp==0):
+                    if jTmp - eTmp == 0:
                         break
-                    colJ = ''.join([pattern[i][jTmp] for i in range(len(pattern))])
-                    colE = ''.join([pattern[i][eTmp] for i in range(len(pattern))])
+                    colJ = "".join([pattern[i][jTmp] for i in range(len(pattern))])
+                    colE = "".join([pattern[i][eTmp] for i in range(len(pattern))])
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         j += 1
     if mirrorFound:
-        mirrors['v']=cols[-1]+1
+        mirrors["v"] = cols[-1] + 1
     return mirrors
 
-if __name__=='__main__': 
-    test =False
-    if test:
-        filename = "day13-test-input.txt"
-    else:
-        filename = "day13-1-input.txt"
-    lines = read_file(filename)
-    patterns= parseInformation(lines)
+
+def summarizeNotes(patterns):
     net = 0
     for pattern in patterns:
         mirrors = findMirror(pattern)
-        # print(pattern,mirrors)
-        max = ['x',-float('inf')]
+        max = ["x", -float("inf")]
         for m in mirrors:
-            if mirrors[m]> max[1]:
+            if mirrors[m] > max[1]:
                 max = [m, mirrors[m]]
-        if max[0]=='v':
+        if max[0] == "v":
             net += max[1]
         else:
-            net += 100*max[1]
-    print(net)
+            net += 100 * max[1]
+    return net
+
+
+def main(filename):
+    patterns = parseInformation(filename)
+    net = summarizeNotes(patterns)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 13, [405], main)
+    else:
+        ans = getAnswer(2023, 13, main)
+        print("The sum of possible arrangements is: {0}".format(ans))
--- 2023/day13/day13-2.py
+++ 2023/day13/day13-2.py
@@ -1,13 +1,18 @@
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
-    patterns =[]
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    patterns = []
     tmp = []
     # Read lines and expand by rows
     for line in lines:
-        if line.strip()=='':
+        if line.strip() == "":
             patterns.append(tmp)
             tmp = []
         else:
@@ -15,182 +20,198 @@ def parseInformation(lines):
     patterns.append(tmp)
     return patterns
 
+
 def compare(str1, str2):
     difs = 0
-    for (idx, value) in enumerate(str1):
+    for idx, value in enumerate(str1):
         if value != str2[idx]:
             difs += 1
-    if difs==1:
+    if difs == 1:
         return True
     return False
+
 
 def findMirror(pattern):
     i = 0
     mirrorFound = False
     mirrors = {}
-    while i<len(pattern)-1:
-        e = len(pattern)-1
-        while (e>i):
-            if pattern[i]==pattern[e] and (i==0 or e==len(pattern)-1):
+    lines = []
+    while i < len(pattern) - 1:
+        e = len(pattern) - 1
+        while e > i:
+            if pattern[i] == pattern[e] and (i == 0 or e == len(pattern) - 1):
                 iTmp = i
-                lines = []
-                while (pattern[iTmp]==pattern[e]):
-                    if (e-iTmp)==1:
+                while pattern[iTmp] == pattern[e]:
+                    if (e - iTmp) == 1:
                         lines.append(iTmp)
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         lines.append(iTmp)
                     iTmp += 1
                     e -= 1
-                    if (iTmp-e==0):
+                    if iTmp - e == 0:
                         break
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         i += 1
     if mirrorFound:
-        mirrors['h'] = lines[-1]+1
+        mirrors["h"] = lines[-1] + 1
     j = 0
     mirrorFound = False
-    while j<len(pattern[0])-1:
-        e = len(pattern[0])-1
-        while (e>j):
-            colJ = ''.join([pattern[i][j] for i in range(len(pattern))])
-            colE = ''.join([pattern[i][e] for i in range(len(pattern))])
-            if colJ==colE and (j==0 or e==len(pattern[0])-1):
+    cols = []
+    while j < len(pattern[0]) - 1:
+        e = len(pattern[0]) - 1
+        while e > j:
+            colJ = "".join([pattern[i][j] for i in range(len(pattern))])
+            colE = "".join([pattern[i][e] for i in range(len(pattern))])
+            if colJ == colE and (j == 0 or e == len(pattern[0]) - 1):
                 jTmp = j
-                cols = []
-                while (colJ==colE):
-                    if (e-jTmp)==1 :
+                while colJ == colE:
+                    if (e - jTmp) == 1:
                         cols.append(jTmp)
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         cols.append(jTmp)
                     jTmp += 1
                     e -= 1
-                    if (jTmp-e==0):
+                    if jTmp - e == 0:
                         break
-                    colJ = ''.join([pattern[i][jTmp] for i in range(len(pattern))])
-                    colE = ''.join([pattern[i][e] for i in range(len(pattern))])
+                    colJ = "".join([pattern[i][jTmp] for i in range(len(pattern))])
+                    colE = "".join([pattern[i][e] for i in range(len(pattern))])
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         j += 1
     if mirrorFound:
-        mirrors['v']=cols[-1]+1
+        mirrors["v"] = cols[-1] + 1
     return mirrors
+
 
 def findMirrorSmidge(pattern, mirrors):
     i = 0
     mirrorFound = False
     mirrorsSmidge = {}
-    while i<len(pattern)-1:
-        e = len(pattern)-1
-        while (e>i):
+    lines = []
+    while i < len(pattern) - 1:
+        e = len(pattern) - 1
+        while e > i:
             comp = compare(pattern[i], pattern[e])
             used = False
-            if (comp or pattern[i]==pattern[e]) and (i==0 or e==len(pattern)-1):
+            if (comp or pattern[i] == pattern[e]) and (i == 0 or e == len(pattern) - 1):
                 iTmp = i
                 eTmp = e
-                lines = []
-                while ((comp and not(used))  or pattern[iTmp]==pattern[eTmp]):
-                    if comp and pattern[iTmp]!=pattern[eTmp]:
+                while (comp and not (used)) or pattern[iTmp] == pattern[eTmp]:
+                    if comp and pattern[iTmp] != pattern[eTmp]:
                         used = True
-                    if (eTmp-iTmp)==1:
+                    if (eTmp - iTmp) == 1:
                         lines.append(iTmp)
-                        if 'h' in mirrors:
-                            if mirrors['h']== lines[-1]+1:
+                        if "h" in mirrors:
+                            if mirrors["h"] == lines[-1] + 1:
                                 break
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         lines.append(iTmp)
                     iTmp += 1
                     eTmp -= 1
                     comp = compare(pattern[iTmp], pattern[eTmp])
-                    if (iTmp-eTmp==0):
+                    if iTmp - eTmp == 0:
                         break
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         i += 1
     if mirrorFound:
-        mirrorsSmidge['h'] = lines[-1]+1
+        mirrorsSmidge["h"] = lines[-1] + 1
     j = 0
     mirrorFound = False
-    while j<len(pattern[0])-1:
-        e = len(pattern[0])-1
-        while (e>j):
-            colJ = ''.join([pattern[i][j] for i in range(len(pattern))])
-            colE = ''.join([pattern[i][e] for i in range(len(pattern))])
+    cols = []
+    while j < len(pattern[0]) - 1:
+        e = len(pattern[0]) - 1
+        while e > j:
+            colJ = "".join([pattern[i][j] for i in range(len(pattern))])
+            colE = "".join([pattern[i][e] for i in range(len(pattern))])
             comp = compare(colJ, colE)
             used = False
-            if (comp or colJ==colE) and (j==0 or e==len(pattern[0])-1):
+            if (comp or colJ == colE) and (j == 0 or e == len(pattern[0]) - 1):
                 jTmp = j
                 eTmp = e
-                cols = []
-                while ((comp and not(used)) or colJ==colE):
-                    if comp and colJ!=colE:
+                while (comp and not (used)) or colJ == colE:
+                    if comp and colJ != colE:
                         used = True
-                    if (eTmp-jTmp)==1 :
+                    if (eTmp - jTmp) == 1:
                         cols.append(jTmp)
-                        if 'v' in mirrors:
-                            if mirrors['v']== cols[-1]+1:
+                        if "v" in mirrors:
+                            if mirrors["v"] == cols[-1] + 1:
                                 break
-                        mirrorFound =True
+                        mirrorFound = True
                         break
                     else:
                         cols.append(jTmp)
                     jTmp += 1
                     eTmp -= 1
-                    if (jTmp-eTmp==0):
+                    if jTmp - eTmp == 0:
                         break
-                    colJ = ''.join([pattern[i][jTmp] for i in range(len(pattern))])
-                    colE = ''.join([pattern[i][eTmp] for i in range(len(pattern))])
+                    colJ = "".join([pattern[i][jTmp] for i in range(len(pattern))])
+                    colE = "".join([pattern[i][eTmp] for i in range(len(pattern))])
                     comp = compare(colJ, colE)
             if mirrorFound:
                 break
-            e -= 1 
+            e -= 1
         if mirrorFound:
             break
         j += 1
     if mirrorFound:
-        mirrorsSmidge['v']=cols[-1]+1
+        mirrorsSmidge["v"] = cols[-1] + 1
     return mirrorsSmidge
 
-if __name__=='__main__': 
-    test = False
-    if test:
-        filename = "day13-test-input.txt"
-    else:
-        filename = "day13-1-input.txt"
-    lines = read_file(filename)
-    patterns= parseInformation(lines)
+
+def summarizeNotes(patterns):
     net = 0
     for pattern in patterns:
         mirrors = findMirror(pattern)
         mirrorsSmidge = findMirrorSmidge(pattern, mirrors)
-        max = ['x',0]
+        max = ["x", 0]
         for m in mirrorsSmidge:
             if m in mirrors:
-                if mirrorsSmidge[m]!=mirrors[m]:
-                    if mirrorsSmidge[m]> max[1]:
+                if mirrorsSmidge[m] != mirrors[m]:
+                    if mirrorsSmidge[m] > max[1]:
                         max = [m, mirrorsSmidge[m]]
             else:
-                if mirrorsSmidge[m]> max[1]:
+                if mirrorsSmidge[m] > max[1]:
                     max = [m, mirrorsSmidge[m]]
-        if max[1]==0:
-            print(d,mirrorsSmidge, mirrors)
-        if max[0]=='v':
+        if max[0] == "v":
             net += max[1]
         else:
-            net += 100*max[1]
-    print(net)
+            net += 100 * max[1]
+    return net
+
+
+def main(filename):
+    patterns = parseInformation(filename)
+    net = summarizeNotes(patterns)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 13, [400], main)
+    else:
+        ans = getAnswer(2023, 13, main)
+        print("The sum of possible arrangements is: {0}".format(ans))
