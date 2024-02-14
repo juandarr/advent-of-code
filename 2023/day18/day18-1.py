a8ad7935826ffb122d17900327b45aae62edb8f2
Juan Ríos <juanrios@tuta.io>
2024-02-14 13:39:31 -0500

    refactor: adapted solutions of day 18 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day18/day18-1.py
+++ 2023/day18/day18-1.py
@@ -1,50 +1,57 @@
-from time import sleep, time
+from os.path import dirname, abspath
+import sys
 import heapq
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     m = []
     for line in lines:
         m.append(list(line.strip().split()))
     return m
 
-'''
-Finds when a given edge location/region is the entry/exit point to the interior
-'''
-def isWallPresent(i,j,ground):
-    arc = [0,0]
-    if i+1< len(ground):
-        if ground[i+1][j]=='#':
-            arc[1]=1
-    if i-1 >=0:
-        if ground[i-1][j]=='#':
-            arc[0]=1
-    while (ground[i][j]=='#'):
-        j+=1
-        if (j>=len(ground[0])):
-            break
-    j -=1
-    if i+1< len(ground):
-        if ground[i+1][j]=='#':
-            arc[1]=1
-    if i-1 >=0:
-        if ground[i-1][j]=='#':
-            arc[0]=1
-    if arc==[1,1]:
-        return True,j
-    return False,j
 
-def dig(start,m, groundDug):
+"""
+Finds when a given edge location/region is the entry/exit point to the interior
+"""
+
+
+def isWallPresent(i, j, ground):
+    arc = [0, 0]
+    if i + 1 < len(ground):
+        if ground[i + 1][j] == "#":
+            arc[1] = 1
+    if i - 1 >= 0:
+        if ground[i - 1][j] == "#":
+            arc[0] = 1
+    while ground[i][j] == "#":
+        j += 1
+        if j >= len(ground[0]):
+            break
+    j -= 1
+    if i + 1 < len(ground):
+        if ground[i + 1][j] == "#":
+            arc[1] = 1
+    if i - 1 >= 0:
+        if ground[i - 1][j] == "#":
+            arc[0] = 1
+    if arc == [1, 1]:
+        return True, j
+    return False, j
+
+
+def dig(start, m, groundDug):
     # Find the ground edge based on intructions
     groundDug.add(start)
-    i,j = start
-    lim_i =[0,0] 
-    lim_j =[0,0]
-    dir = {'R':(0,1),'L':(0,-1), 'U':(-1,0), 'D':(1,0)}
+    i, j = start
+    lim_i = [0, 0]
+    lim_j = [0, 0]
+    dir = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
     for instruction in m:
         direction, value, color = instruction
         d = dir[direction]
@@ -52,62 +59,68 @@ def dig(start,m, groundDug):
         for _ in range(value):
             i += d[0]
             j += d[1]
-            groundDug.add((i,j))
-            if (i<lim_i[0]):
+            groundDug.add((i, j))
+            if i < lim_i[0]:
                 lim_i[0] = i
-            elif (i>lim_i[1]):
+            elif i > lim_i[1]:
                 lim_i[1] = i
-            if (j<lim_j[0]):
+            if j < lim_j[0]:
                 lim_j[0] = j
-            elif (j>lim_j[1]):
+            elif j > lim_j[1]:
                 lim_j[1] = j
     # Create the ground edge in a matrix form
     ground = []
     i_calibration = -lim_i[0]
     j_calibration = -lim_j[0]
-    for i in range(lim_i[0]+i_calibration,lim_i[1]+1+i_calibration):
+    for i in range(lim_i[0] + i_calibration, lim_i[1] + 1 + i_calibration):
         ground.append([])
-        for j in range(lim_j[0]+j_calibration, lim_j[1]+1+j_calibration):
-            ground[-1].append('.')
+        for j in range(lim_j[0] + j_calibration, lim_j[1] + 1 + j_calibration):
+            ground[-1].append(".")
     for s in groundDug:
-        ground[s[0]+i_calibration][s[1]+j_calibration] = '#'
-    for g in ground:
-        print(''.join(g))
+        ground[s[0] + i_calibration][s[1] + j_calibration] = "#"
     # Fill the ground
-    i = 0 
+    i = 0
     count = 0
-    while (i<len(ground)):
+    while i < len(ground):
         j = 0
         open = False
         tmp = 0
-        while (j<len(ground[0])):
+        while j < len(ground[0]):
             if open:
-                if ground[i][j]=='.':
-                    tmp +=1
-                if ground[i][j]=='#':
-                    wallPresent, j = isWallPresent(i,j,ground)
+                if ground[i][j] == ".":
+                    tmp += 1
+                if ground[i][j] == "#":
+                    wallPresent, j = isWallPresent(i, j, ground)
                     if wallPresent:
                         open = False
                         count += tmp
             else:
-                if ground[i][j]=='#':
-                    wallPresent, j = isWallPresent(i,j,ground)
+                if ground[i][j] == "#":
+                    wallPresent, j = isWallPresent(i, j, ground)
                     if wallPresent:
                         open = True
                         tmp = 0
-            j+=1
-        i+= 1
-    return count+len(groundDug)
+            j += 1
+        i += 1
+    return count + len(groundDug)
 
-if __name__=='__main__': 
-    test = True
-    testNumber = 1
-    if test:
-        filename = "day18-test{0}-input.txt".format(testNumber)
+
+def main(filename):
+    m = parseInformation(filename)
+    ans = dig((0, 0), m, set())
+    return ans
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day18-1-input.txt"
-    lines = read_file(filename)
-    m = parseInformation(lines)
-    groundDug = set()
-    area = dig((0,0), m, groundDug)
-    print(area)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 18, [62], main)
+    else:
+        ans = getAnswer(2023, 18, main)
+        print("The volume of lava that can be held is: {0}".format(ans))
--- 2023/day18/day18-2.py
+++ 2023/day18/day18-2.py
@@ -1,36 +1,46 @@
-from time import sleep, time
-import heapq
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     m = []
     for line in lines:
         m.append(list(line.strip().split()))
     return m
 
-def shoelace(groundDug):
-    i = len(groundDug)-2 
-    total = 0
-    while (i>=0):
-        total += groundDug[i][0]*groundDug[i-1][1]-groundDug[i][1]*groundDug[i-1][0]
-        i-=1
-        if (i==0):
-            total += groundDug[i][0]*groundDug[len(groundDug)-2][1]-groundDug[i][1]*groundDug[len(groundDug)-2][0]
-            break
-    return total//2
 
-def dig(start,m, groundDug):
+def shoelace(groundDug):
+    i = len(groundDug) - 2
+    total = 0
+    while i >= 0:
+        total += (
+            groundDug[i][0] * groundDug[i - 1][1]
+            - groundDug[i][1] * groundDug[i - 1][0]
+        )
+        i -= 1
+        if i == 0:
+            total += (
+                groundDug[i][0] * groundDug[len(groundDug) - 2][1]
+                - groundDug[i][1] * groundDug[len(groundDug) - 2][0]
+            )
+            break
+    return total // 2
+
+
+def dig(start, m, groundDug):
     # Find the ground edge based on intructions
-    groundDug[0]=start
-    i,j = start
-    dir = {'R':(0,1),'L':(0,-1), 'U':(-1,0), 'D':(1,0)}
-    vToDir = {'0':'R', '1':'D','2':'L','3':'U'}
-    hexToDec = {'a':10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15}
-    c =  1
+    groundDug[0] = start
+    i, j = start
+    dir = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
+    vToDir = {"0": "R", "1": "D", "2": "L", "3": "U"}
+    hexToDec = {"a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15}
+    c = 1
     edge = 0
     for instruction in m:
         # Uncomment to solutions to challenge 1
@@ -43,39 +53,47 @@ def dig(start,m, groundDug):
         d = dir[vToDir[hexValue[-2]]]
         hexValue = hexValue[2:-2]
         value = 0
-        l = len(hexValue)-1
-        for idx in range(len(hexValue)-1,-1,-1):
+        h = len(hexValue) - 1
+        for idx in range(len(hexValue) - 1, -1, -1):
             if hexValue[idx] in hexToDec:
-                value += hexToDec[hexValue[idx]]*(16**(l-idx))
+                value += hexToDec[hexValue[idx]] * (16 ** (h - idx))
             else:
-                value += int(hexValue[idx])*(16**(l-idx))
+                value += int(hexValue[idx]) * (16 ** (h - idx))
         # End: part 2 specific
-        i += d[0]*value
-        j += d[1]*value
-        edge+=value
-        groundDug[c]=(i,j)
-        c +=1
+        i += d[0] * value
+        j += d[1] * value
+        edge += value
+        groundDug[c] = (i, j)
+        c += 1
     count = 1
     for g in groundDug:
-        if g+1 in groundDug:
-            if (groundDug[g][0]==groundDug[g+1][0]):
-                if groundDug[g][1]>groundDug[g+1][1]: 
-                    count += groundDug[g][1]-groundDug[g+1][1]
-            elif (groundDug[g][1]==groundDug[g+1][1]):
-                if groundDug[g][0]<groundDug[g+1][0]:
-                    count += groundDug[g+1][0]-groundDug[g][0]
+        if g + 1 in groundDug:
+            if groundDug[g][0] == groundDug[g + 1][0]:
+                if groundDug[g][1] > groundDug[g + 1][1]:
+                    count += groundDug[g][1] - groundDug[g + 1][1]
+            elif groundDug[g][1] == groundDug[g + 1][1]:
+                if groundDug[g][0] < groundDug[g + 1][0]:
+                    count += groundDug[g + 1][0] - groundDug[g][0]
     area = shoelace(groundDug)
     return area + count
 
-if __name__=='__main__': 
-    test = False
-    testNumber = 1
-    if test:
-        filename = "day18-test{0}-input.txt".format(testNumber)
+
+def main(filename):
+    m = parseInformation(filename)
+    ans = dig((0, 0), m, {})
+    return ans
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day18-1-input.txt"
-    lines = read_file(filename)
-    m = parseInformation(lines)
-    groundDug = {}
-    area = dig((0,0), m, groundDug)
-    print(area)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 18, [952408144115], main)
+    else:
+        ans = getAnswer(2023, 18, main)
+        print("The volume of lava that can be held is: {0}".format(ans))
