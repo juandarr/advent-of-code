688e1b8ddda3d2aa8018747e6f03c48e6a0e4ba9
Juan Ríos <juanrios@tuta.io>
2024-02-15 23:56:35 -0500

    refactor: adapted solutions of day 19 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day19/day19-1.py
+++ 2023/day19/day19-1.py
@@ -1,66 +1,77 @@
-from time import sleep, time
-import heapq
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     wf = {}
     parts = []
-    c= 0
-    while lines[c]!='\n':
-        tmp =lines[c].strip().split('{')
+    c = 0
+    while lines[c] != "\n":
+        tmp = lines[c].strip().split("{")
         name = tmp[0]
         tmp = tmp[1][:-1]
-        tmp = tmp.split(',')
+        tmp = tmp.split(",")
         tmpAr = []
         for i in tmp:
-            tmpAr.append(tuple(i.split(':')))
+            tmpAr.append(tuple(i.split(":")))
         wf[name] = tmpAr
-        c+=1
-    c+=1
+        c += 1
+    c += 1
     for line in lines[c:]:
-        tmp = line.strip()[1:-1].split(',')
+        tmp = line.strip()[1:-1].split(",")
         tmpAr = {}
         for i in tmp:
-            tmp_i = i.split('=')
-            tmpAr[tmp_i[0]]=int(tmp_i[1])
+            tmp_i = i.split("=")
+            tmpAr[tmp_i[0]] = int(tmp_i[1])
         parts.append(tmpAr)
-    return  wf,parts
+    return wf, parts
 
-def workflows(wf,parts):
+
+def workflows(wf, parts):
     net = 0
     for part in parts:
-        cur = 'in'
-        x =part['x']
-        m = part['m']
-        a = part['a']
-        s = part['s']
-        while cur not in ['A','R']:
+        cur = "in"
+        x = part["x"]
+        m = part["m"]
+        a = part["a"]
+        s = part["s"]
+        while cur not in ["A", "R"]:
             default = True
             for rule in wf[cur][:-1]:
                 if eval(rule[0]):
                     cur = rule[1]
-                    default=False
+                    default = False
                     break
             if default:
                 cur = wf[cur][-1][0]
-        if cur=='A':
+        if cur == "A":
             for k in part:
                 net += part[k]
-    return net 
+    return net
 
-if __name__=='__main__': 
-    test = False
-    testNumber = 1
-    if test:
-        filename = "day19-test{0}-input.txt".format(testNumber)
+
+def main(filename):
+    wf, parts = parseInformation(filename)
+    net = workflows(wf, parts)
+    return net
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day19-1-input.txt"
-    lines = read_file(filename)
-    wf,parts = parseInformation(lines)
-    net = workflows(wf,parts)
-
-    print(net)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 19, [19114], main)
+    else:
+        ans = getAnswer(2023, 19, main)
+        print("The number of distinct combinations of ratings is: {0}".format(ans))
--- 2023/day19/day19-2.py
+++ 2023/day19/day19-2.py
@@ -1,74 +1,88 @@
-from time import sleep, time
-from math import factorial as f
-import heapq
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     wf = {}
-    c= 0
-    while lines[c]!='\n':
-        tmp =lines[c].strip().split('{')
+    c = 0
+    while lines[c] != "\n":
+        tmp = lines[c].strip().split("{")
         name = tmp[0]
         tmp = tmp[1][:-1]
-        tmp = tmp.split(',')
+        tmp = tmp.split(",")
         tmpAr = []
+        r = []
         for i in tmp[:-1]:
-            iTmp = i.split(':')
+            iTmp = i.split(":")
             node = iTmp[0][0]
-            if (len(iTmp[0].split('>'))>1):
-                r = [int(iTmp[0].split('>')[1])+1, 4000]
-            elif (len(iTmp[0].split('<'))>1):
-                r = [1,int(iTmp[0].split('<')[1])-1]
+            if len(iTmp[0].split(">")) > 1:
+                r = [int(iTmp[0].split(">")[1]) + 1, 4000]
+            elif len(iTmp[0].split("<")) > 1:
+                r = [1, int(iTmp[0].split("<")[1]) - 1]
             tmpAr.append(((node, r), iTmp[1]))
         tmpAr.append((tmp[-1],))
         wf[name] = tmpAr
-        c+=1
-    return  wf
+        c += 1
+    return wf
+
 
 def exploreWorkflows(wfs, wf, ranges, nodes):
-    if wf in ['R','A']:
-        if wf=='A':
+    if wf in ["R", "A"]:
+        if wf == "A":
             c = 1
             for r in ranges:
-                c *= (r[1]-r[0]+1)
+                c *= r[1] - r[0] + 1
             return c
         return 0
     rules = wfs[wf]
     net = 0
     for rule in rules:
-        if len(rule)>1:
+        if len(rule) > 1:
             node = nodes[rule[0][0]]
-            r = rule[0][1] 
+            r = rule[0][1]
             n = rule[1]
             include = ranges[node].copy()
             exclude = ranges[node].copy()
-            if r[0]>ranges[node][0]:
+            if r[0] > ranges[node][0]:
                 include[0] = r[0]
-                exclude[1] =r[0]-1 
-            if r[1]<ranges[node][1]:
+                exclude[1] = r[0] - 1
+            if r[1] < ranges[node][1]:
                 include[1] = r[1]
-                exclude[0] = r[1]+1
+                exclude[0] = r[1] + 1
             ranges[node] = include
-            net += exploreWorkflows(wfs,n, ranges.copy(),nodes)
+            net += exploreWorkflows(wfs, n, ranges.copy(), nodes)
             ranges[node] = exclude
         else:
             n = rule[0]
-            net += exploreWorkflows(wfs,n, ranges.copy(),nodes)
-    return net 
+            net += exploreWorkflows(wfs, n, ranges.copy(), nodes)
+    return net
 
-if __name__=='__main__': 
-    test = False
-    testNumber = 1
-    if test:
-        filename = "day19-test{0}-input.txt".format(testNumber)
+
+def main(filename):
+    wfs = parseInformation(filename)
+    nodes = {"x": 0, "m": 1, "a": 2, "s": 3}
+    possibilities = exploreWorkflows(
+        wfs, "in", [[1, 4000], [1, 4000], [1, 4000], [1, 4000]], nodes
+    )
+    return possibilities
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day19-1-input.txt"
-    lines = read_file(filename)
-    nodes = {'x':0,'m':1 , 'a':2, 's':3}
-    wfs = parseInformation(lines)
-    possibilities = exploreWorkflows(wfs, 'in',[[1,4000],[1,4000],[1,4000],[1,4000]], nodes)
-    print(possibilities)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 19, [167409079868000], main)
+    else:
+        ans = getAnswer(2023, 19, main)
+        print("The number of distinct combinations of ratings is: {0}".format(ans))
