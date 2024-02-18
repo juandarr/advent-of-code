67c40bc609e47f6b49cc2fb020ed68818521a63a
Juan Ríos <juanrios@tuta.io>
2024-02-17 21:04:34 -0500

    refactor: adapted solutions of day 22 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day22/day22-1.py
+++ 2023/day22/day22-1.py
@@ -1,75 +1,91 @@
-from time import sleep, time
-from copy import deepcopy
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     bricks = {}
     c = 1
     for line in lines:
-        points = line.strip().split('~')
+        points = line.strip().split("~")
         p = []
         for point in points:
-            x,y,z = point.split(',')
+            x, y, z = point.split(",")
             p.append([int(x), int(y), int(z)])
-        bricks[c] =[[p[0][0], p[1][0]], [p[0][1], p[1][1]], [p[0][2], p[1][2]]]
-        c+=1
-    return  bricks
+        bricks[c] = [[p[0][0], p[1][0]], [p[0][1], p[1][1]], [p[0][2], p[1][2]]]
+        c += 1
+    return bricks
+
 
 def printLevel(lev, bricks, maxLevel):
-    labels = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6: 'F', 7:'G', 8:'H' , 9:'I', 10:'J', 11:'K'}
+    labels = {
+        1: "A",
+        2: "B",
+        3: "C",
+        4: "D",
+        5: "E",
+        6: "F",
+        7: "G",
+        8: "H",
+        9: "I",
+        10: "J",
+        11: "K",
+    }
     c = 0
     mx = []
     my = []
-    while c<=maxLevel:
-        mx.append( list('.'*20))
-        my.append( list('.'*20))
+    while c <= maxLevel:
+        mx.append(list("." * 20))
+        my.append(list("." * 20))
         if c in lev:
-            for l in lev[c]:
-                for x in range(bricks[l][0][0], bricks[l][0][1]+1):
-                    if mx[-1][x] != '.': 
-                        mx[-1][x] = '?'
+            for i in lev[c]:
+                for x in range(bricks[i][0][0], bricks[i][0][1] + 1):
+                    if mx[-1][x] != ".":
+                        mx[-1][x] = "?"
                     else:
-                        mx[-1][x] = labels[l]
-                for y in range(bricks[l][1][0], bricks[l][1][1]+1):
-                    if my[-1][y] != '.': 
-                        my[-1][y] = '?'
+                        mx[-1][x] = labels[i]
+                for y in range(bricks[i][1][0], bricks[i][1][1] + 1):
+                    if my[-1][y] != ".":
+                        my[-1][y] = "?"
                     else:
-                        my[-1][y] = labels[l]
-        c+=1
-    print('\nx vs z : \n')
+                        my[-1][y] = labels[i]
+        c += 1
+    print("\nx vs z : \n")
     for i in reversed(mx):
-        print(''.join(i))
-    print('\ny vs z : \n')
+        print("".join(i))
+    print("\ny vs z : \n")
     for i in reversed(my):
-        print(''.join(i))
+        print("".join(i))
 
-def exploreFall(bricks,test):
+
+def exploreFall(bricks, test):
     lev = {}
-    minLev = float('inf') 
-    maxLev = -float('inf') 
+    minLev = float("inf")
+    maxLev = -float("inf")
     for label in bricks:
         brick = bricks[label]
-        levels = list(range(brick[2][0], brick[2][1]+1))
-        minLev = min(minLev,brick[2][0])
+        levels = list(range(brick[2][0], brick[2][1] + 1))
+        minLev = min(minLev, brick[2][0])
         maxLev = max(maxLev, brick[2][1])
         for level in levels:
-            if level in lev :
+            if level in lev:
                 lev[level][label] = True
             else:
                 lev[level] = {label: True}
-    if test:
-        print('Initial state: ',lev)
-        printLevel(lev, bricks,maxLev)
+    # if test:
+    #     print("Initial state: ", lev)
+    #     printLevel(lev, bricks, maxLev)
     movingDown = True
-    while (movingDown) :
+    while movingDown:
         movingDown = False
         curLevel = 2
-        while curLevel<=maxLev:
-            belowLevel = curLevel -1
+        while curLevel <= maxLev:
+            belowLevel = curLevel - 1
             goingDown = []
             if curLevel in lev:
                 for b in lev[curLevel]:
@@ -80,85 +96,99 @@ def exploreFall(bricks,test):
                             brickBelow = bricks[bBelow]
                             blocked = [False, False]
                             for idx in range(2):
-                                m = list(range(max(brick[idx][0],brickBelow[idx][0]), min(brick[idx][1], brickBelow[idx][1])+1))
-                                if m!=[]:
-                                    blocked[idx]= True
-                                if blocked[0]==True and blocked[1]==True:
-                                    goesBelow =False
+                                m = list(
+                                    range(
+                                        max(brick[idx][0], brickBelow[idx][0]),
+                                        min(brick[idx][1], brickBelow[idx][1]) + 1,
+                                    )
+                                )
+                                if m != []:
+                                    blocked[idx] = True
+                                if blocked[0] is True and blocked[1] is True:
+                                    goesBelow = False
                                     break
-                            if not(goesBelow):
+                            if not (goesBelow):
                                 break
                     if goesBelow:
-                        movingDown  = True
-                        l = curLevel
-                        while (l<=maxLev):
-                            if l in lev:
-                                if b in lev[l]:
-                                    goingDown.append((b,l))
+                        movingDown = True
+                        i = curLevel
+                        while i <= maxLev:
+                            if i in lev:
+                                if b in lev[i]:
+                                    goingDown.append((b, i))
                             else:
                                 break
-                            l +=1
-            if len(goingDown)>0:
+                            i += 1
+            if len(goingDown) > 0:
                 for val in goingDown:
                     b = val[0]
                     level = val[1]
-                    lowLevel = level-1
+                    lowLevel = level - 1
                     del lev[level][b]
-                    if lev[level]=={}:
+                    if lev[level] == {}:
                         del lev[level]
                     if lowLevel in lev:
-                        lev[lowLevel][b] =True
+                        lev[lowLevel][b] = True
                     else:
-                        lev[lowLevel] = {b:True}
-            curLevel +=1
-    if test:
-        print('End state: ',lev)
-        printLevel(lev, bricks,maxLev)
+                        lev[lowLevel] = {b: True}
+            curLevel += 1
+    # if test:
+    #     print("End state: ", lev)
+    #     printLevel(lev, bricks, maxLev)
     c = 1
-    counter =0
-    while (c+1 in lev):
+    counter = 0
+    while c + 1 in lev:
         for b in lev[c]:
             toTest = lev[c].copy()
             del toTest[b]
-            for bAbove in lev[c+1]:
+            goesBelow = True
+            for bAbove in lev[c + 1]:
                 brick = bricks[bAbove]
                 goesBelow = True
                 for bBelow in toTest:
                     brickBelow = bricks[bBelow]
                     blocked = [False, False]
                     for idx in range(2):
-                        m = list(range(max(brick[idx][0],brickBelow[idx][0]), min(brick[idx][1], brickBelow[idx][1])+1))
-                        if m!=[]:
-                            blocked[idx] = True 
-                    if blocked[0]==True and blocked[1]==True:
-                        goesBelow =False 
+                        m = list(
+                            range(
+                                max(brick[idx][0], brickBelow[idx][0]),
+                                min(brick[idx][1], brickBelow[idx][1]) + 1,
+                            )
+                        )
+                        if m != []:
+                            blocked[idx] = True
+                    if blocked[0] is True and blocked[1] is True:
+                        goesBelow = False
                         break
                 else:
                     break
-            if not(goesBelow):
-                counter +=1
-        c +=1
-    counter+=len(lev[c])
-    return  counter
+            if not (goesBelow):
+                counter += 1
+        c += 1
+    counter += len(lev[c])
+    return counter
 
-if __name__=='__main__': 
-    test =False
-    testNumber =7 
-    '''
-    Answers to tests
-    1: 5
-    2: 3
-    3: 2 
-    4: 4
-    5: 3
-    6: 2
-    7: 3
-    '''
-    if test:
-        filename = "day22-test{0}-input.txt".format(testNumber)
-    else:
-        filename = "day22-1-input.txt"
-    lines = read_file(filename)
-    bricks = parseInformation(lines)
+
+def main(filename):
+    bricks = parseInformation(filename)
     p = exploreFall(bricks, test)
-    print(p)
+    return p
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 22, [5, 3, 2, 4, 3, 2, 3], main)
+    else:
+        ans = getAnswer(2023, 22, main)
+        print(
+            "The number of bricks to be safely chosen to be disintegrated is: {0}".format(
+                ans
+            )
+        )
--- 2023/day22/day22-2.py
+++ 2023/day22/day22-2.py
@@ -1,60 +1,77 @@
-from time import sleep, time
 from copy import deepcopy
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and expand by rows
     bricks = {}
     c = 1
     for line in lines:
-        points = line.strip().split('~')
+        points = line.strip().split("~")
         p = []
         for point in points:
-            x,y,z = point.split(',')
+            x, y, z = point.split(",")
             p.append([int(x), int(y), int(z)])
-        bricks[c] =[[p[0][0], p[1][0]], [p[0][1], p[1][1]], [p[0][2], p[1][2]]]
-        c+=1
-    return  bricks
+        bricks[c] = [[p[0][0], p[1][0]], [p[0][1], p[1][1]], [p[0][2], p[1][2]]]
+        c += 1
+    return bricks
+
 
 def printLevel(lev, bricks, maxLevel):
-    labels = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6: 'F', 7:'G', 8:'H' , 9:'I', 10:'J', 11:'K'}
+    labels = {
+        1: "A",
+        2: "B",
+        3: "C",
+        4: "D",
+        5: "E",
+        6: "F",
+        7: "G",
+        8: "H",
+        9: "I",
+        10: "J",
+        11: "K",
+    }
     c = 0
     mx = []
     my = []
-    while c<=maxLevel:
-        mx.append( list('.'*20))
-        my.append( list('.'*20))
+    while c <= maxLevel:
+        mx.append(list("." * 20))
+        my.append(list("." * 20))
         if c in lev:
-            for l in lev[c]:
-                for x in range(bricks[l][0][0], bricks[l][0][1]+1):
-                    if mx[-1][x] != '.': 
-                        mx[-1][x] = '?'
+            for i in lev[c]:
+                for x in range(bricks[i][0][0], bricks[i][0][1] + 1):
+                    if mx[-1][x] != ".":
+                        mx[-1][x] = "?"
                     else:
-                        mx[-1][x] = labels[l]
-                for y in range(bricks[l][1][0], bricks[l][1][1]+1):
-                    if my[-1][y] != '.': 
-                        my[-1][y] = '?'
+                        mx[-1][x] = labels[i]
+                for y in range(bricks[i][1][0], bricks[i][1][1] + 1):
+                    if my[-1][y] != ".":
+                        my[-1][y] = "?"
                     else:
-                        my[-1][y] = labels[l]
-        c+=1
-    print('\nx vs z : \n')
+                        my[-1][y] = labels[i]
+        c += 1
+    print("\nx vs z : \n")
     for i in reversed(mx):
-        print(''.join(i))
-    print('\ny vs z : \n')
+        print("".join(i))
+    print("\ny vs z : \n")
     for i in reversed(my):
-        print(''.join(i))
+        print("".join(i))
 
-def fallSimulation(lev, maxLev):
+
+def fallSimulation(bricks, lev, maxLev):
     movingDown = True
     down = set()
-    while (movingDown) :
+    while movingDown:
         movingDown = False
         curLevel = 2
-        while curLevel<=maxLev:
-            belowLevel = curLevel -1
+        while curLevel <= maxLev:
+            belowLevel = curLevel - 1
             goingDown = []
             if curLevel in lev:
                 for b in lev[curLevel]:
@@ -65,70 +82,76 @@ def fallSimulation(lev, maxLev):
                             brickBelow = bricks[bBelow]
                             blocked = [False, False]
                             for idx in range(2):
-                                m = list(range(max(brick[idx][0],brickBelow[idx][0]), min(brick[idx][1], brickBelow[idx][1])+1))
-                                if m!=[]:
-                                    blocked[idx]= True
-                                if blocked[0]==True and blocked[1]==True:
-                                    goesBelow =False
+                                m = list(
+                                    range(
+                                        max(brick[idx][0], brickBelow[idx][0]),
+                                        min(brick[idx][1], brickBelow[idx][1]) + 1,
+                                    )
+                                )
+                                if m != []:
+                                    blocked[idx] = True
+                                if blocked[0] is True and blocked[1] is True:
+                                    goesBelow = False
                                     break
-                            if not(goesBelow):
+                            if not (goesBelow):
                                 break
                     if goesBelow:
-                        movingDown  = True
-                        l = curLevel
-                        while (l<=maxLev):
-                            if l in lev:
-                                if b in lev[l]:
-                                    goingDown.append((b,l))
+                        movingDown = True
+                        i = curLevel
+                        while i <= maxLev:
+                            if i in lev:
+                                if b in lev[i]:
+                                    goingDown.append((b, i))
                             else:
                                 break
-                            l +=1
-            if len(goingDown)>0:
+                            i += 1
+            if len(goingDown) > 0:
                 for val in goingDown:
                     down.add(val[0])
                     b = val[0]
                     level = val[1]
-                    lowLevel = level-1
+                    lowLevel = level - 1
                     del lev[level][b]
-                    if lev[level]=={}:
+                    if lev[level] == {}:
                         del lev[level]
                     if lowLevel in lev:
-                        lev[lowLevel][b] =True
+                        lev[lowLevel][b] = True
                     else:
-                        lev[lowLevel] = {b:True}
-            curLevel +=1
+                        lev[lowLevel] = {b: True}
+            curLevel += 1
     return len(down)
 
-def exploreFall(bricks,test):
+
+def exploreFall(bricks, test):
     lev = {}
-    minLev = float('inf') 
-    maxLev = -float('inf') 
+    minLev = float("inf")
+    maxLev = -float("inf")
     for label in bricks:
         brick = bricks[label]
-        levels = list(range(brick[2][0], brick[2][1]+1))
-        minLev = min(minLev,brick[2][0])
+        levels = list(range(brick[2][0], brick[2][1] + 1))
+        minLev = min(minLev, brick[2][0])
         maxLev = max(maxLev, brick[2][1])
         for level in levels:
-            if level in lev :
+            if level in lev:
                 lev[level][label] = True
             else:
                 lev[level] = {label: True}
-    if test:
-        print('Initial state: ',lev)
-        printLevel(lev, bricks,maxLev)
-    fallSimulation(lev, maxLev)
-    if test:
-        print('End state: ',lev)
-        printLevel(lev, bricks,maxLev)
+    # if test:
+    #     print("Initial state: ", lev)
+    #     printLevel(lev, bricks, maxLev)
+    fallSimulation(bricks, lev, maxLev)
+    # if test:
+    #     print("End state: ", lev)
+    #     printLevel(lev, bricks, maxLev)
     c = 1
     simulate = []
-    while (c+1 in lev):
+    while c + 1 in lev:
         for b in lev[c]:
             toTest = lev[c].copy()
             del toTest[b]
             goesBelow = False
-            for bAbove in lev[c+1]:
-                if b==bAbove:
+            for bAbove in lev[c + 1]:
+                if b == bAbove:
                     continue
                 brick = bricks[bAbove]
                 goesBelow = True
@@ -136,17 +159,22 @@ def exploreFall(bricks,test):
                     brickBelow = bricks[bBelow]
                     blocked = [False, False]
                     for idx in range(2):
-                        m = list(range(max(brick[idx][0],brickBelow[idx][0]), min(brick[idx][1], brickBelow[idx][1])+1))
-                        if m!=[]:
-                            blocked[idx] = True 
-                    if blocked[0]==True and blocked[1]==True:
-                        goesBelow =False 
+                        m = list(
+                            range(
+                                max(brick[idx][0], brickBelow[idx][0]),
+                                min(brick[idx][1], brickBelow[idx][1]) + 1,
+                            )
+                        )
+                        if m != []:
+                            blocked[idx] = True
+                    if blocked[0] is True and blocked[1] is True:
+                        goesBelow = False
                         break
                 else:
                     break
             if goesBelow:
-                simulate.append((b,c))
-        c +=1
+                simulate.append((b, c))
+        c += 1
     net = 0
     for s in simulate:
         levToSimulate = deepcopy(lev)
@@ -154,24 +182,31 @@ def exploreFall(bricks,test):
         c = s[1]
         while b in levToSimulate[c]:
             del levToSimulate[c][b]
-            if levToSimulate[c]=={}:
+            if levToSimulate[c] == {}:
                 del levToSimulate[c]
-            c+=1
-        net+=fallSimulation(levToSimulate, maxLev)
+            c += 1
+        net += fallSimulation(bricks, levToSimulate, maxLev)
     return net
 
-if __name__=='__main__': 
-    test = False
-    testNumber =1 
-    '''
-    Answers to tests
-    1: 7
-    '''
-    if test:
-        filename = "day22-test{0}-input.txt".format(testNumber)
-    else:
-        filename = "day22-1-input.txt"
-    lines = read_file(filename)
-    bricks = parseInformation(lines)
+
+def main(filename):
+    bricks = parseInformation(filename)
     p = exploreFall(bricks, test)
-    print(p)
+    return p
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 22, [7], main, test=["1"])
+    else:
+        ans = getAnswer(2023, 22, main)
+        print(
+            "The sum of the number of other bricks that would fall is: {0}".format(ans)
+        )
