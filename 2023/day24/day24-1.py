f94cf132275880b91d7bfda9cb8b67c413154efe
Juan Ríos <juanrios@tuta.io>
2024-02-19 15:21:36 -0500

    refactor: adapted solutions of day 24 challenges, aoc 2023 to new script structure and requirements (aoc and utils in root folder)

--- 2023/day24/day24-1.py
+++ 2023/day24/day24-1.py
@@ -1,57 +1,73 @@
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+from os.path import dirname, abspath
+import sys
 
-def parseInformation(lines):
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and extract every position and velocity for each hailstone
     hailstones = []
     for line in lines:
-        tmp = line.strip().split('@')
-        tmp = {'pos': [int(i) for i in  tmp[0].strip().split(',')], 'vel': [int(i) for i in  tmp[1].strip().split(',')]}
+        tmp = line.strip().split("@")
+        tmp = {
+            "pos": [int(i) for i in tmp[0].strip().split(",")],
+            "vel": [int(i) for i in tmp[1].strip().split(",")],
+        }
         hailstones.append(tmp)
     return hailstones
 
+
 def findEstimate(hailstones, limits):
     i = 0
-    c= 0
-    while i<len(hailstones)-1:
+    c = 0
+    while i < len(hailstones) - 1:
         p1 = hailstones[i]
-        j = i+1
-        while j<len(hailstones):
+        j = i + 1
+        while j < len(hailstones):
             p2 = hailstones[j]
-            m = -1*p2['vel'][1]/p2['vel'][0]
-            num = m*(p2['pos'][0]-p1['pos'][0])+p2['pos'][1]-p1['pos'][1]
-            den = p1['vel'][1]+m*p1['vel'][0]
-            if den!=0: 
-                t1 = num/den
-                num = p1['vel'][1]*t1-p2['pos'][1]+p1['pos'][1]
-                den = p2['vel'][1]
-                if den!=0:
-                    t2 = num/den
-                    if (t1>0 and t2>0):
-                        x = p1['vel'][0]*t1+p1['pos'][0]
-                        y = p1['vel'][1]*t1+p1['pos'][1]
-                        if x>=limits[0] and x<=limits[1] and y>=limits[0] and y<=limits[1]:
-                            c+=1
-            j +=1
-        i+=1
+            m = -1 * p2["vel"][1] / p2["vel"][0]
+            num = m * (p2["pos"][0] - p1["pos"][0]) + p2["pos"][1] - p1["pos"][1]
+            den = p1["vel"][1] + m * p1["vel"][0]
+            if den != 0:
+                t1 = num / den
+                num = p1["vel"][1] * t1 - p2["pos"][1] + p1["pos"][1]
+                den = p2["vel"][1]
+                if den != 0:
+                    t2 = num / den
+                    if t1 > 0 and t2 > 0:
+                        x = p1["vel"][0] * t1 + p1["pos"][0]
+                        y = p1["vel"][1] * t1 + p1["pos"][1]
+                        if (
+                            x >= limits[0]
+                            and x <= limits[1]
+                            and y >= limits[0]
+                            and y <= limits[1]
+                        ):
+                            c += 1
+            j += 1
+        i += 1
     return c
 
-if __name__=='__main__': 
-    test = False
-    testNumber =1 
-    '''
-    Answers to tests
-    1: 2 
-    '''
-    if test:
-        filename = "day24-test{0}-input.txt".format(testNumber)
+
+def main(filename, limits):
+    hailstones = parseInformation(filename)
+    c = findEstimate(hailstones, limits)
+    return c
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day24-1-input.txt"
-    lines = read_file(filename)
-    hailstones = parseInformation(lines)
-    # Test
-    # limits = [7,27]
-    limits= [200000000000000, 400000000000000]
-    c= findEstimate(hailstones,limits)
-    print('Estimate: ', c)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 24, [2], main, [7, 27])
+    else:
+        ans = getAnswer(2023, 24, main, [200000000000000, 400000000000000])
+        print("The number of intersections within the test area is: {0}".format(ans))
--- 2023/day24/day24-2.py
+++ 2023/day24/day24-2.py
@@ -1,103 +1,115 @@
 import numpy as np
+from os.path import dirname, abspath
+import sys
 
-def read_file(filename):
-    file = open(filename,'r')
-    return file.readlines()
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
 
-def parseInformation(lines):
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
     # Read lines and extract every position and velocity for each hailstone
     hailstones = []
     for line in lines:
-        tmp = line.strip().split('@')
-        tmp = {'pos': [int(i) for i in  tmp[0].strip().split(',')], 'vel': [int(i) for i in  tmp[1].strip().split(',')]}
+        tmp = line.strip().split("@")
+        tmp = {
+            "pos": [int(i) for i in tmp[0].strip().split(",")],
+            "vel": [int(i) for i in tmp[1].strip().split(",")],
+        }
         hailstones.append(tmp)
     return hailstones
 
+
 def findEstimate(hailstones):
-    A = np.zeros((6,6))
-    B = np.zeros((6,1))
+    A = np.zeros((6, 6))
+    B = np.zeros((6, 1))
     points = {}
     c = 1
     for hailstone in hailstones:
         points[c] = hailstone
-        c+=1
-        if c==4:
+        c += 1
+        if c == 4:
             break
-    p1 = points[1]['pos']
-    p2 = points[2]['pos']
-    p3 = points[3]['pos']
-    v1 = points[1]['vel']
-    v2 = points[2]['vel']
-    v3 = points[3]['vel']
+    p1 = points[1]["pos"]
+    p2 = points[2]["pos"]
+    p3 = points[3]["pos"]
+    v1 = points[1]["vel"]
+    v2 = points[2]["vel"]
+    v3 = points[3]["vel"]
 
-    A[0,0] =v2[1]-v1[1]
-    A[0,1] =v1[0]-v2[0]
-    A[0,2] =0
-    A[0,3] =p1[1]-p2[1]
-    A[0,4] =p2[0]-p1[0]
-    A[0,5] =0
+    A[0, 0] = v2[1] - v1[1]
+    A[0, 1] = v1[0] - v2[0]
+    A[0, 2] = 0
+    A[0, 3] = p1[1] - p2[1]
+    A[0, 4] = p2[0] - p1[0]
+    A[0, 5] = 0
 
-    A[1,0] =v2[2]-v1[2]
-    A[1,1] =0
-    A[1,2] =v1[0]-v2[0]
-    A[1,3] =p1[2]-p2[2]
-    A[1,4] =0
-    A[1,5] =p2[0]-p1[0]
+    A[1, 0] = v2[2] - v1[2]
+    A[1, 1] = 0
+    A[1, 2] = v1[0] - v2[0]
+    A[1, 3] = p1[2] - p2[2]
+    A[1, 4] = 0
+    A[1, 5] = p2[0] - p1[0]
 
-    A[2,0] =0
-    A[2,1] =v2[2]-v1[2]
-    A[2,2] =v1[1]-v2[1]
-    A[2,3] =0
-    A[2,4] =p1[2]-p2[2]
-    A[2,5] =p2[1]-p1[1]
+    A[2, 0] = 0
+    A[2, 1] = v2[2] - v1[2]
+    A[2, 2] = v1[1] - v2[1]
+    A[2, 3] = 0
+    A[2, 4] = p1[2] - p2[2]
+    A[2, 5] = p2[1] - p1[1]
 
+    A[3, 0] = v3[1] - v1[1]
+    A[3, 1] = v1[0] - v3[0]
+    A[3, 2] = 0
+    A[3, 3] = p1[1] - p3[1]
+    A[3, 4] = p3[0] - p1[0]
+    A[3, 5] = 0
 
-    A[3,0] =v3[1]-v1[1]
-    A[3,1] =v1[0]-v3[0]
-    A[3,2] =0
-    A[3,3] =p1[1]-p3[1]
-    A[3,4] =p3[0]-p1[0]
-    A[3,5] =0
+    A[4, 0] = v3[2] - v1[2]
+    A[4, 1] = 0
+    A[4, 2] = v1[0] - v3[0]
+    A[4, 3] = p1[2] - p3[2]
+    A[4, 4] = 0
+    A[4, 5] = p3[0] - p1[0]
 
-    A[4,0] =v3[2]-v1[2]
-    A[4,1] =0
-    A[4,2] =v1[0]-v3[0]
-    A[4,3] =p1[2]-p3[2]
-    A[4,4] =0
-    A[4,5] =p3[0]-p1[0]
+    A[5, 0] = 0
+    A[5, 1] = v3[2] - v1[2]
+    A[5, 2] = v1[1] - v3[1]
+    A[5, 3] = 0
+    A[5, 4] = p1[2] - p3[2]
+    A[5, 5] = p3[1] - p1[1]
 
-    A[5,0] =0
-    A[5,1] =v3[2]-v1[2]
-    A[5,2] =v1[1]-v3[1]
-    A[5,3] =0
-    A[5,4] =p1[2]-p3[2]
-    A[5,5] =p3[1]-p1[1]
+    B[0, 0] = p2[0] * v2[1] - p2[1] * v2[0] - p1[0] * v1[1] + p1[1] * v1[0]
+    B[1, 0] = p2[0] * v2[2] - p2[2] * v2[0] - p1[0] * v1[2] + p1[2] * v1[0]
+    B[2, 0] = p2[1] * v2[2] - p2[2] * v2[1] - p1[1] * v1[2] + p1[2] * v1[1]
+    B[3, 0] = p3[0] * v3[1] - p3[1] * v3[0] - p1[0] * v1[1] + p1[1] * v1[0]
+    B[4, 0] = p3[0] * v3[2] - p3[2] * v3[0] - p1[0] * v1[2] + p1[2] * v1[0]
+    B[5, 0] = p3[1] * v3[2] - p3[2] * v3[1] - p1[1] * v1[2] + p1[2] * v1[1]
 
-    B[0,0] = p2[0]*v2[1]-p2[1]*v2[0]-p1[0]*v1[1]+p1[1]*v1[0]
-    B[1,0] = p2[0]*v2[2]-p2[2]*v2[0]-p1[0]*v1[2]+p1[2]*v1[0]
-    B[2,0] = p2[1]*v2[2]-p2[2]*v2[1]-p1[1]*v1[2]+p1[2]*v1[1]
-    B[3,0] = p3[0]*v3[1]-p3[1]*v3[0]-p1[0]*v1[1]+p1[1]*v1[0]
-    B[4,0] = p3[0]*v3[2]-p3[2]*v3[0]-p1[0]*v1[2]+p1[2]*v1[0]
-    B[5,0] = p3[1]*v3[2]-p3[2]*v3[1]-p1[1]*v1[2]+p1[2]*v1[1]
-
-    s = np.linalg.solve(A,B)
+    s = np.linalg.solve(A, B)
     ans = 0
     for i in range(3):
-        ans +=round(s[i,0])
+        ans += round(s[i, 0])
     return ans
 
-if __name__=='__main__': 
-    test =False
-    testNumber =1 
-    '''
-    Answers to tests
-    1: 47 
-    '''
-    if test:
-        filename = "day24-test{0}-input.txt".format(testNumber)
+
+def main(filename):
+    hailstones = parseInformation(filename)
+    c = findEstimate(hailstones)
+    return c
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
     else:
-        filename = "day24-1-input.txt"
-    lines = read_file(filename)
-    hailstones = parseInformation(lines)
-    ans= findEstimate(hailstones)
-    print(ans)
+        raise Exception('Wrong argument, expected "test" or "main"')
+    if test:
+        performTests(2023, 24, [47], main)
+    else:
+        ans = getAnswer(2023, 24, main)
+        print("The number of intersections within the test area is: {0}".format(ans))
