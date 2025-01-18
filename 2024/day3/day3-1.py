f9f92cbc66711159b31f2273fb78b0d681f7fca7
Juan Rios <juandarr@proton.me>
2025-01-18 15:48:48 -0500

    add: solution to challenge 1, day 3 of 2024 aoc

---
+++ 2024/day3/day3-1.py
@@ -0,0 +1,46 @@
+from os.path import dirname, abspath
+import sys
+import re
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    str = file.read()
+    return str
+
+def findOcurrences(s):
+    pattern = r"mul\(\d+,\d+\)"
+    ocurrences = re.findall(pattern, s, flags=0)
+    return ocurrences
+
+def multiplication(s):
+    ocurrences = findOcurrences(s)
+    total = 0
+    for o in ocurrences:
+        tmp = o[:-1].split('(')[1].split(',')
+        total += int(tmp[0])*int(tmp[1])
+    return total
+
+def main(filename):
+    str = parseInformation(filename)
+    total = multiplication(str)
+    return total
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2024, 3, [161], main)
+    else:
+        total = getAnswer(2024, 3, main)
+        print("The multiplication of valid patterns is: {0}".format(total))
