06a48c58e8953b9e32fb56c7afcf02073a50a472
Juan Rios <juanrios@tuta.io>
2025-09-23 23:25:19 -0500

    add: solution to problem 2 of day 2, aoc 2015

---
+++ 2015/day2/day2-2.py
@@ -0,0 +1,43 @@
+from os.path import dirname, abspath
+import sys
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    values = file.read()
+    rows = values.rstrip().split('\n')
+    return rows
+
+
+def totalRibbon(rows):
+    total = 0
+    for row in rows:
+        dim = row.split('x')
+        a,b,c = map(lambda d: int(d), dim)
+        halfPerimeter = [a+b,a+c,b+c]
+        total += 2*min(halfPerimeter)+ a*b*c
+    return total 
+
+def main(filename):
+    rows = parseInformation(filename)
+    totalArea = totalRibbon(rows)
+    return totalArea
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2015, 2, [48], main)
+    else:
+        totalArea = getAnswer(2015, 2, main)
+        print("The total surface area required of wrapping paper is {0}".format(totalArea))
