089ca29f4d9201928f8c3e7dcd10cd3121d5160c
Juan Rios <juanrios@tuta.io>
2025-09-30 23:56:02 -0500

    add: solution to problem 2 of day 5, aoc 2015, about finding the total brightness of a grid of 1000x1000 lights

---
+++ 2015/day6/day6-2.py
@@ -0,0 +1,64 @@
+from os.path import dirname, abspath
+import sys
+import hashlib
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    data = file.read()
+    rawInstructions = data.rstrip().split('\n')
+    instructions = [[1 if 'turn on' in i else (0 if 'turn off' in i else -1), i.split(' through ')[0].split(' ')[-1],i.split(' through ')[-1]] for i in rawInstructions]
+    return instructions
+
+
+def computeLights(instructions):
+    on = {}
+    for inst in instructions:
+        x1,y1 = map(lambda d: int(d),inst[1].split(',') )
+        x2,y2 = map(lambda d: int(d),inst[2].split(',') )
+        for x in range(x1,x2+1):
+            for y in range(y1,y2+1):
+                if (x,y) in on:
+                    if inst[0]==0:
+                        on[(x,y)] -= 1
+                    elif inst[0]==1:
+                        on[(x,y)] +=1
+                    else:
+                        on[(x,y)] +=2
+                    if on[(x,y)]==0:
+                        del on[(x,y)]
+                else:
+                    if inst[0]==0:
+                        pass
+                    elif inst[0]==1:
+                        on[(x,y)]=1
+                    else:
+                        on[(x,y)]=2
+    total = 0
+    for k in on:
+        total += on[k]
+    return total
+
+def main(filename):
+    instructions = parseInformation(filename)
+    totalBrightness = computeLights(instructions)
+    return totalBrightness
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2015, 6, [10**6,2*10**3,0], main)
+    else:
+        totalBrightness = getAnswer(2015, 6, main)
+        print("The total brightness of all lights is {0}".format(totalBrightness))
+
