97f95021da2ec349056ef86f5d97da2175e1a5da
Juan Rios <juanrios@tuta.io>
2025-10-08 02:21:22 -0500

    update: solution to challenge 1 in progress. Updated input data files

--- 2015/day8/day8-1.py
+++ 2015/day8/day8-1.py
@@ -1,6 +1,6 @@
 from os.path import dirname, abspath
 import sys
-import hashlib
+import re
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from utils import performTests, getAnswer  # noqa E402
@@ -15,7 +15,21 @@ def parseInformation(filename):
 
 
 def getDiff(strs):
-    return 0
+    totalR =0
+    totalM = 0
+    for s in strs: 
+        totalR += len(s)
+        totalM += len(s)-2
+        tmp = s[1:-1]
+        rawStrs = [r'\\\\',r'\\\"',r'(\\\\x[0-9,a,b,c,d,e,f]{2}|\w{1}\\x[0-9,a,b,c,d,e,f]{2}|^\\x[0-9,a,b,c,d,e,f]{2})']
+        for rule,rawStr in enumerate(rawStrs):
+            positions = re.findall(rawStr,tmp)
+            print(tmp, positions)
+            if rule==2:
+                totalM -= 3*len(positions)
+            else:
+                totalM -=1*len(positions)
+    return totalR-totalM
 
 def main(filename):
     strs = parseInformation(filename)
@@ -37,3 +51,6 @@ if __name__ == "__main__":
         dif = getAnswer(2015, 8, main)
         print("The difference between number of characters of code and characters in memory is {0}".format(dif))
 
+'''
+Wrong: 1387, 1380, 1146, 1378, 1335, 1341
+'''
