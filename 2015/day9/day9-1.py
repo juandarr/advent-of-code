380ee0077ed03127ce2d736ea0b3e91855a06878
Juan Rios <juanrios@tuta.io>
2025-10-12 01:25:46 -0500

    feat: completed solutions to challenge 1, advent of code 2015, day 9, about finding the shortest travel vising every node in the network

--- 2015/day9/day9-1.py
+++ 2015/day9/day9-1.py
@@ -9,13 +9,45 @@ from utils import performTests, getAnswer  # noqa E402
 def parseInformation(filename):
     file = open(filename, "r")
     data = file.read()
-    routes = data.rstrip().split('\n')
-    print(routes)
+    routesRaw = [row.split(' = ') for row in data.rstrip().split('\n')]
+    routes = [(row[0].split(' to '), int(row[-1])) for row in routesRaw]
     return routes
 
 
 def shortestDistance(routes):
-    return 0
+    paths = {}
+    nodes = set()
+    d = float('inf')
+    for route in routes:
+        if route[0][0] in paths:
+            paths[route[0][0]].append((route[0][1], route[1]))
+        else:
+            paths[route[0][0]]= [(route[0][1], route[1])]
+        if route[0][1] in paths:
+            paths[route[0][1]].append((route[0][0], route[1]))
+        else:
+            paths[route[0][1]]= [(route[0][0], route[1])]
+        nodes.add(route[0][0])
+        nodes.add(route[0][1])
+    for start in nodes:
+        toExpand = [([start],{start:1}, 0)]
+        while len(toExpand)>0:
+            cur = toExpand.pop()
+            if len(cur[0])==len(nodes):
+                if d>cur[2]:
+                    d = cur[2]
+                continue
+            if cur[0][-1] in paths:
+                for p in paths[cur[0][-1]]:
+                    if p[0] not in cur[1]:
+                        s = cur[1].copy()
+                        s[p[0]]=1
+                        toExpand.append((cur[0]+[p[0]], s , cur[2]+p[1]))
+                    else:
+                        continue
+            else:
+                continue
+    return d 
 
 def main(filename):
     routes = parseInformation(filename)
