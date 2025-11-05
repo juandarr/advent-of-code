c87294c516494500925464b5d09d26823635735c
Juan Rios <juanrios@tuta.io>
2025-11-04 23:59:30 -0500

    add: draft solution to challenge 1, day 12 of aoc 2025, about finding the sum of numbers in a json format string

---
+++ 2015/day12/day12-1.py
@@ -0,0 +1,36 @@
+from os.path import dirname, abspath
+import sys
+import re
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    data = file.read()
+    return data.rstrip() 
+
+def addNumbers(s):
+    return 0
+
+def main(filename):
+    s = parseInformation(filename)
+    print(s)
+    net = addNumbers(s)
+    return net
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2015, 12, [12,6,0,0], main)
+    else:
+        iterations = getAnswer(2015, 12, main)
+        print("Given current Santa's password next password is {0}".format(iterations))
