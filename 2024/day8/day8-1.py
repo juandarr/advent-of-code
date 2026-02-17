b70e9834c1fe325db5e8fab177e4ed43c2bb9401
Juan Rios <juanrios@tuta.io>
2026-02-17 00:00:33 -0500

    add: draft with input parse of challenge 1, day 8, aoc 2024

---
+++ 2024/day8/day8-1.py
@@ -0,0 +1,39 @@
+from os.path import dirname, abspath
+import sys
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    str = file.read()
+    rows = str.split('\n')
+    m = []
+    for row in rows:
+        m.append(row.split())
+    return m
+
+def checkRow():
+   pass
+
+def main(filename):
+    m= parseInformation(filename)
+    points= checkRow()
+    return points
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2024, 8, [0],main)
+    else:
+        total = getAnswer(2024, 8, main)
+        print("The result is: {0}".format(total))
