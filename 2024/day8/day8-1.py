49b7b42be7d35881adcfc0c34fad70ecae148da8
Juan Rios <juanrios@tuta.io>
2026-02-17 23:34:38 -0500

    feat: completed solution to challenge 1 of day 8, aoc 2024

--- 2024/day8/day8-1.py
+++ 2024/day8/day8-1.py
@@ -12,16 +12,42 @@ def parseInformation(filename):
     m = []
     for row in rows:
         m.append(list(row))
-    print(m)
-    return m
+    antenas = {}
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if m[i][j]!='.':
+                if m[i][j] in antenas:
+                    antenas[m[i][j]].append((i,j))
+                else:
+                    antenas[m[i][j]] = [(i,j)]
+    return m, antenas
 
-def checkRow():
-   pass
+def checkMap(m, antenas):
+    antinodes = {}
+    iLimit = len(m)
+    jLimit = len(m[0])
+    for freq in antenas:
+        l = len(antenas[freq])
+        for i in range(l):
+            a = antenas[freq][i]
+            for k in range(i+1,l):
+                b = antenas[freq][k]
+                dia = a[0]-b[0]
+                dja = a[1]-b[1]
+                nodes = [(a[0]+dia, a[1]+dja)]
+                dib = b[0]-a[0]
+                djb = b[1]-a[1]
+                nodes.append((b[0]+dib, b[1]+djb))
+                for node in nodes:
+                    if node[0]>=0 and node[0]<iLimit and node[1]>=0 and node[1]<jLimit:
+                        if node not in antinodes:
+                            antinodes[node] = 1
+    return len(antinodes)
 
 def main(filename):
-    m= parseInformation(filename)
-    points= checkRow()
-    return points
+    m,antenas= parseInformation(filename)
+    locations= checkMap(m,antenas)
+    return locations
 
 
 if __name__ == "__main__":
@@ -34,7 +60,7 @@ if __name__ == "__main__":
         raise Exception('Wrong argument, expected "test" or "main"')
 
     if test:
-        performTests(2024, 8, [0],main)
+        performTests(2024, 8, [14,4],main)
     else:
         total = getAnswer(2024, 8, main)
-        print("The result is: {0}".format(total))
+        print("The number of unique antinode locations is: {0}".format(total))
