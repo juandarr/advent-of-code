c9ff72b433501458230a0e4119440dbfafbd4677
Juan Rios <juanrios@tuta.io>
2026-02-24 00:15:11 -0500

    feat: completed challenge 2 of day 10, aoc 2024. Finding the sum of ratings of all trailheads

---
+++ 2024/day10/day10-2.py
@@ -0,0 +1,62 @@
+from os.path import dirname, abspath
+import sys
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+from utils import performTests, getAnswer  # noqa E402
+
+
+def parseInformation(filename):
+    file = open(filename, "r")
+    str = file.read()
+    rows = str.split('\n')
+    return [list(row) for row in  rows]
+
+def traverseMap(pos, rows):
+    toExpand = [pos]
+    score = 0
+    dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+    while len(toExpand)>0:
+        p = toExpand.pop()
+        for d in dirs:
+            i = p[0]+d[0]
+            j = p[1]+d[1]
+            if (i<0 or i>=len(rows) or j<0 or j>=len(rows[0])): 
+                continue
+            if rows[i][j]==rows[p[0]][p[1]]+1:
+                if rows[i][j]==9:
+                    score += 1
+                else:
+                    toExpand.append((i,j))
+    return score
+
+def checkScoreSum(rows):
+    startSet = []
+    for i,row in enumerate(rows):
+        for j,d in enumerate(row):
+            rows[i][j] = int(rows[i][j])
+            if d=='0':
+                startSet.append((i,j))
+    totalScore = 0
+    for start in startSet:
+        totalScore += traverseMap(start, rows)
+    return totalScore
+
+def main(filename):
+    rows = parseInformation(filename)
+    totalScore= checkScoreSum(rows)
+    return totalScore
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "test":
+        test = True
+    elif args[0] == "main":
+        test = False
+    else:
+        raise Exception('Wrong argument, expected "test" or "main"')
+
+    if test:
+        performTests(2024, 10, [81],main)
+    else:
+        total = getAnswer(2024, 10, main)
+        print("The sum of ratings of all trailheads is: {0}".format(total))
